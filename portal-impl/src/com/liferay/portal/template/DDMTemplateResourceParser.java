71a6a00a884761fff5be6ba3416a20e22d3eca56
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-10-06 11:27:44 +0200

    Drop support for velocity template loading

--- portal-impl/src/com/liferay/portal/template/DDMTemplateResourceParser.java
+++ portal-impl/src/com/liferay/portal/template/DDMTemplateResourceParser.java
@@ -37,8 +37,7 @@ import com.liferay.portal.kernel.util.StringPool;
  */
 @OSGiBeanProperties(
 	property = {
-		"lang.type=" + TemplateConstants.LANG_TYPE_FTL,
-		"lang.type=" + TemplateConstants.LANG_TYPE_VM
+		"lang.type=" + TemplateConstants.LANG_TYPE_FTL
 	},
 	service = TemplateResourceParser.class
 )
