23acab86b4617f1daf746328f9d445cb7e1121d4
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-12-20 14:12:35 +0100

    WIP

--- modules/apps/foundation/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/servlet/taglib/soy/base/BaseChartTag.java
+++ modules/apps/foundation/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/servlet/taglib/soy/base/BaseChartTag.java
@@ -21,9 +21,14 @@ import com.liferay.portal.kernel.servlet.taglib.util.OutputData;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 import javax.servlet.ServletRequest;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author Chema Balsas
@@ -118,21 +123,22 @@ public abstract class BaseChartTag extends TemplateRendererTag {
 		String svgPath = npmResolver.resolveModuleName(
 			"clay-charts/src/svg/patterns.svg");
 
-		StringBundler sb = new StringBundler();
+		Bundle bundle = FrameworkUtil.getBundle(BaseChartTag.class);
 
-		String svg = "<svg xmlns=\"http://www.w3.org/2000/svg\"><defs><!-- circles --><pattern id=\"circles\" patternUnits=\"userSpaceOnUse\" width=\"16\" height=\"16\" style=\"fill:rgba(0,0,0,0.3)\"><circle cx=\"4\" cy=\"4\" r=\"2\"/><circle cx=\"12\" cy=\"12\" r=\"2\"/></pattern>" +
-"<!-- diagonal left large --><pattern id=\"diagonal-left-large\" patternUnits=\"userSpaceOnUse\" width=\"10\" height=\"10\" style=\"fill:rgba(0,0,0,0.3)\"><polygon points=\"9.3,0 10,0.7 10,0 \"/><polygon points=\"0,0 0,0.7 9.3,10 10,10 10,9.3 0.7,0 \"/><polygon points=\"0,9.3 0,10 0.7,10 \"/></pattern>" +
-"<!-- diagonal left small --><pattern id=\"diagonal-left-small\" patternUnits=\"userSpaceOnUse\" width=\"5\" height=\"5\" style=\"fill:rgba(0,0,0,0.3)\"><polygon points=\"5,0 4.3,0 5,0.7 \"/><polygon points=\"0.7,0 0,0 0,0.7 4.3,5 5,5 5,4.3 \"/><polygon points=\"0,4.3 0,5 0.7,5 \"/></pattern>" +
-"<!-- diagonal right large --><pattern id=\"diagonal-right-large\" patternUnits=\"userSpaceOnUse\" width=\"10\" height=\"10\" style=\"fill:rgba(0,0,0,0.3)\"><polygon points=\"10,9.3 9.3,10 10,10 \"/><polygon points=\"10,0 9.3,0 0,9.3 0,10 0.7,10 10,0.7 \"/><polygon points=\"0.7,0 0,0 0,0.7 \"/></pattern>" +
-"<!-- diagonal right small --><pattern id=\"diagonal-right-small\" patternUnits=\"userSpaceOnUse\" width=\"5\" height=\"5\" style=\"fill:rgba(0,0,0,0.3)\"><polygon points=\"5,4.3 4.3,5 5,5 \"/><polygon points=\"5,0 4.3,0 0,4.3 0,5 0.7,5 5,0.7 \"/><polygon points=\"0.7,0 0,0 0,0.7 \"/></pattern>" +
-"<!-- horizontal large --><pattern id=\"horizontal-large\" patternUnits=\"userSpaceOnUse\" width=\"8\" height=\"8\" style=\"fill:rgba(0,0,0,0.3)\"><rect y=\"7\" width=\"8\" height=\"1\"/></pattern>" +
-"<!-- horizontal small --><pattern id=\"horizontal-small\" patternUnits=\"userSpaceOnUse\" width=\"4\" height=\"4\" style=\"fill:rgba(0,0,0,0.3)\"><rect y=\"3\" width=\"4\" height=\"1\"/></pattern>" +
-"<!-- vertical large --><pattern id=\"vertical-large\" patternUnits=\"userSpaceOnUse\" width=\"8\" height=\"8\" style=\"fill:rgba(0,0,0,0.3)\"><rect x=\"7\" y=\"0\" width=\"1\" height=\"8\"/></pattern>" +
-"<!-- vertical small --><pattern id=\"vertical-small\" patternUnits=\"userSpaceOnUse\" width=\"4\" height=\"4\" style=\"fill:rgba(0,0,0,0.3)\"><rect x=\"3\" width=\"1\" height=\"4\"/></pattern></defs></svg>";
+		URL url = bundle.getEntry("META-INF/resources/node_modules/" + svgPath);
 
-		sb.append(svg);
+		try {
+			String svg = StringUtil.read(url.openStream());
 
-		outputData.addData(_OUTPUT_SVG_KEY, WebKeys.PAGE_TOP, sb);
+			outputData.addData(
+				_OUTPUT_SVG_KEY,
+				WebKeys.PAGE_TOP,
+				new StringBundler(svg));
+		}
+		catch(IOException ioe) {
+			// TODO: decide what to do if SVG cannot be read
+			XXX
+		}
 	}
 
 	private static final String _OUTPUT_CSS_KEY = BaseChartTag.class.getName() + "_CSS";
