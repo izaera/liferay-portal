66c9c60382ea07d7dc0c073c0b17f3bf83f88ff9
Rodolfo Roza Miranda <rodolfo.roza@liferay.com>
2019-02-08 17:28:41 -0200

    Add service layer interface

---
+++ modules/apps/frontend-taglib/frontend-taglib-clay-attribute-provider-api/src/main/java/com/liferay/frontend/taglib/clay/attribute/provider/ClayComponentAttributeProvider.java
@@ -0,0 +1,10 @@
+package com.liferay.frontend.taglib.clay.attribute.provider;
+
+import java.util.Map;
+
+/**
+ * @author Rodolfo Roza Miranda
+ */
+public interface ClayComponentAttributeProvider {
+	public Map<String, Object> getAttributes();
+}
---
+++ modules/apps/frontend-taglib/frontend-taglib-clay-attribute-provider-api/src/main/java/com/liferay/frontend/taglib/clay/attribute/provider/ClayComponentAttributeProviderRegistry.java
@@ -0,0 +1,8 @@
+package com.liferay.frontend.taglib.clay.attribute.provider;
+
+/**
+ * @author Rodolfo Roza Miranda
+ */
+public interface ClayComponentAttributeProviderRegistry {
+	public ClayComponentAttributeProvider get(String key);
+}
