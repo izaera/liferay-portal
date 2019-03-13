f26955f41ddfe03d2803522f2b83bef977b630c9
Iván Zaera Avellón <izaera@gmail.com>
2019-03-13 17:20:25 +0100

    LPS-91785 Implement AJAX pagination support in TableTag

---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/internal/AbsolutePortalURLBuilderProvider.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.internal;
+
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilder;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(immediate = true, service = {})
+public class AbsolutePortalURLBuilderProvider {
+
+	public static AbsolutePortalURLBuilder getAbsolutePortalURLBuilder() {
+		if (_absolutePortalURLBuilderProvider == null) {
+			return null;
+		}
+
+		return _absolutePortalURLBuilderProvider._absolutePortalURLBuilder;
+	}
+
+	public AbsolutePortalURLBuilderProvider() {
+		_absolutePortalURLBuilderProvider = this;
+	}
+
+	private static AbsolutePortalURLBuilderProvider
+		_absolutePortalURLBuilderProvider;
+
+	@Reference
+	private AbsolutePortalURLBuilder _absolutePortalURLBuilder;
+
+}
