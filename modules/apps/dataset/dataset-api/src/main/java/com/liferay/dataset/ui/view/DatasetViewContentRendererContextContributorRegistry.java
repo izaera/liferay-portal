a106b2b59a0bd4864d12b5697b6a114f2795062f
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-04-13 10:32:31 +0200

    LPS-130410 Rename method

--- modules/apps/dataset/dataset-api/src/main/java/com/liferay/dataset/ui/view/DatasetViewContentRendererContextContributorRegistry.java
+++ modules/apps/dataset/dataset-api/src/main/java/com/liferay/dataset/ui/view/DatasetViewContentRendererContextContributorRegistry.java
@@ -22,7 +22,7 @@ import java.util.List;
 public interface DatasetViewContentRendererContextContributorRegistry {
 
 	public List<DatasetViewContentRendererContextContributor>
-		getDatasetContentRendererContextContributors(
+		getDatasetViewContentRendererContextContributors(
 			String datasetViewContentRendererName);
 
 }
