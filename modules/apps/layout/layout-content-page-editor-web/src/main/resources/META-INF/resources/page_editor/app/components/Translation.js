6f15ffd494977baa77b40f255bcff4f14dc7d7d2
Patrick Yeo <patrick.yeo@liferay.com>
2021-07-12 12:54:20 -0700

    LPS-132737 layout-content-page-editor-web Page Editor Toolbar isolate component and dropdown menus

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Translation.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/Translation.js
@@ -195,6 +195,9 @@ export default function Translation({
 			hasRightSymbols
 			menuElementAttrs={{
 				className: 'page-editor__translation',
+				containerProps: {
+					className: 'cadmin',
+				},
 			}}
 			onActiveChange={setActive}
 			trigger={
