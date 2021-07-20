c8a4b7248d24b87b30bc8bc0304f9398b4d1b04e
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-07-20 14:31:35 +0200

    LPS-136001 Register JS Portlets

--- modules/apps/remote-app/remote-app-admin-web/src/main/java/com/liferay/remote/app/admin/web/internal/RemoteAppPortletRegistrar.java
+++ modules/apps/remote-app/remote-app-admin-web/src/main/java/com/liferay/remote/app/admin/web/internal/RemoteAppPortletRegistrar.java
@@ -14,13 +14,24 @@
 
 package com.liferay.remote.app.admin.web.internal;
 
+import com.liferay.frontend.js.loader.modules.extender.npm.JSModule;
+import com.liferay.frontend.js.loader.modules.extender.npm.JSPackage;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistry;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistryUpdate;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.remote.app.admin.web.internal.portlet.JSPortlet;
 import com.liferay.remote.app.admin.web.internal.portlet.RemoteAppPortlet;
 import com.liferay.remote.app.model.RemoteAppEntry;
 import com.liferay.remote.app.service.RemoteAppEntryLocalService;
 
+import java.io.InputStream;
+
+import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -36,28 +47,42 @@ import org.osgi.service.component.annotations.Reference;
 @Component(immediate = true, service = RemoteAppPortletRegistrar.class)
 public class RemoteAppPortletRegistrar {
 
-	public void registerPortlet(RemoteAppEntry remoteAppEntry) {
-		_registerPortlet(remoteAppEntry);
+	public void register(RemoteAppEntry remoteAppEntry) {
+		NPMRegistryUpdate update = _npmRegistry.update();
+
+		_register(update, remoteAppEntry);
+
+		update.finish();
 	}
 
-	public void unregisterPortlet(RemoteAppEntry remoteAppEntry) {
-		_unregisterPortlet(remoteAppEntry.getRemoteAppEntryId());
+	public void unregister(RemoteAppEntry remoteAppEntry) {
+		NPMRegistryUpdate update = _npmRegistry.update();
+
+		_unregister(update, remoteAppEntry.getRemoteAppEntryId());
+
+		update.finish();
 	}
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
 		_bundleContext = bundleContext;
 
+		_jsPackage = _npmResolver.getJSPackage();
+
 		if (_log.isInfoEnabled()) {
 			_log.info("Starting remote app entries");
 		}
+
+		NPMRegistryUpdate update = _npmRegistry.update();
 
 		for (RemoteAppEntry remoteAppEntry :
 				remoteAppEntryLocalService.getRemoteAppEntries(
 					QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
 
-			registerPortlet(remoteAppEntry);
+			_register(update, remoteAppEntry);
 		}
+
+		update.finish();
 	}
 
 	@Deactivate
@@ -66,15 +91,84 @@ public class RemoteAppPortletRegistrar {
 			_log.info("Stopping remote app entries");
 		}
 
+		NPMRegistryUpdate update = _npmRegistry.update();
+
 		for (long remoteAppEntryId : _remoteAppPortlets.keySet()) {
-			_unregisterPortlet(remoteAppEntryId);
+			_unregister(update, remoteAppEntryId);
 		}
+
+		update.finish();
 	}
 
 	@Reference
 	protected RemoteAppEntryLocalService remoteAppEntryLocalService;
 
-	private void _registerPortlet(RemoteAppEntry remoteAppEntry) {
+	private static String _loadTemplate(String name) {
+		try (InputStream inputStream =
+				RemoteAppPortletRegistrar.class.getResourceAsStream(
+					"dependencies/" + name)) {
+
+			return StringUtil.read(inputStream);
+		}
+		catch (Exception exception) {
+			_log.error("Unable to read template " + name, exception);
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private void _register(
+		NPMRegistryUpdate update, RemoteAppEntry remoteAppEntry) {
+
+		if ("js-portlet".equals(remoteAppEntry.getType())) {
+			String moduleName =
+				"js-portlet/" + remoteAppEntry.getRemoteAppEntryId();
+
+			_jsModules.put(
+				remoteAppEntry.getRemoteAppEntryId(),
+				update.registerJSModule(
+					_jsPackage, moduleName, Collections.emptyList(),
+					StringUtil.replace(
+						_TPL_JS_PORTLET,
+						new String[] {"[$MODULE$]", "[$PACKAGE$]", "[$URL$]"},
+						new String[] {
+							moduleName, _jsPackage.getResolvedId(),
+							remoteAppEntry.getUrl()
+						}),
+					null));
+
+			_registerJSPortlet(remoteAppEntry, moduleName);
+		}
+		else if ("remote-app".equals(remoteAppEntry.getType())) {
+			_registerRemoteAppPortlet(remoteAppEntry);
+		}
+	}
+
+	private void _registerJSPortlet(
+		RemoteAppEntry remoteAppEntry, String moduleName) {
+
+		JSPortlet jsPortlet = new JSPortlet(
+			_jsPackage, moduleName, remoteAppEntry);
+
+		long remoteAppEntryId = remoteAppEntry.getRemoteAppEntryId();
+
+		JSPortlet existingJSPortlet = _jsPortlets.putIfAbsent(
+			remoteAppEntryId, jsPortlet);
+
+		if (existingJSPortlet != null) {
+			throw new IllegalStateException(
+				"Remote app entry " + remoteAppEntryId +
+					" is already registered");
+		}
+
+		jsPortlet.register(_bundleContext);
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Started js portlet " + jsPortlet.getName());
+		}
+	}
+
+	private void _registerRemoteAppPortlet(RemoteAppEntry remoteAppEntry) {
 		RemoteAppPortlet remoteAppPortlet = new RemoteAppPortlet(
 			remoteAppEntry);
 
@@ -92,11 +186,40 @@ public class RemoteAppPortletRegistrar {
 		remoteAppPortlet.register(_bundleContext);
 
 		if (_log.isInfoEnabled()) {
-			_log.info("Started remote app entry " + remoteAppPortlet.getName());
+			_log.info(
+				"Started remote app portlet " + remoteAppPortlet.getName());
 		}
 	}
 
-	private void _unregisterPortlet(long remoteAppEntryId) {
+	private void _unregister(NPMRegistryUpdate update, long remoteAppEntryId) {
+		JSModule jsModule = _jsModules.remove(remoteAppEntryId);
+
+		if (jsModule != null) {
+			update.unregisterJSModule(jsModule);
+		}
+
+		if (_jsPortlets.containsKey(remoteAppEntryId)) {
+			_unregisterJSPortlet(remoteAppEntryId);
+		}
+
+		if (_remoteAppPortlets.containsKey(remoteAppEntryId)) {
+			_unregisterRemoteAppPortlet(remoteAppEntryId);
+		}
+	}
+
+	private void _unregisterJSPortlet(long remoteAppEntryId) {
+		JSPortlet jsPortlet = _jsPortlets.remove(remoteAppEntryId);
+
+		if (jsPortlet != null) {
+			jsPortlet.unregister();
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Stopped JS portlet " + jsPortlet.getName());
+			}
+		}
+	}
+
+	private void _unregisterRemoteAppPortlet(long remoteAppEntryId) {
 		RemoteAppPortlet remoteAppPortlet = _remoteAppPortlets.remove(
 			remoteAppEntryId);
 
@@ -105,15 +228,30 @@ public class RemoteAppPortletRegistrar {
 
 			if (_log.isInfoEnabled()) {
 				_log.info(
-					"Stopped remote app entry " + remoteAppPortlet.getName());
+					"Stopped remote app portlet " + remoteAppPortlet.getName());
 			}
 		}
 	}
+
+	private static final String _TPL_JS_PORTLET = _loadTemplate(
+		"js-portlet.js.tpl");
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		RemoteAppPortletRegistrar.class);
 
 	private BundleContext _bundleContext;
+	private final ConcurrentMap<Long, JSModule> _jsModules =
+		new ConcurrentHashMap<>();
+	private JSPackage _jsPackage;
+	private final ConcurrentMap<Long, JSPortlet> _jsPortlets =
+		new ConcurrentHashMap<>();
+
+	@Reference
+	private NPMRegistry _npmRegistry;
+
+	@Reference
+	private NPMResolver _npmResolver;
+
 	private final ConcurrentMap<Long, RemoteAppPortlet> _remoteAppPortlets =
 		new ConcurrentHashMap<>();
 
