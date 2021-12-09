119c65283e514964a41e04a1819889309a564be0
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-12-09 10:02:25 +0100

    LPS-142295 regen

--- modules/apps/remote-app/remote-app-api/src/main/java/com/liferay/remote/app/service/RemoteAppEntryLocalServiceWrapper.java
+++ modules/apps/remote-app/remote-app-api/src/main/java/com/liferay/remote/app/service/RemoteAppEntryLocalServiceWrapper.java
@@ -85,13 +85,6 @@ public class RemoteAppEntryLocalServiceWrapper
 		return _remoteAppEntryLocalService.addRemoteAppEntry(remoteAppEntry);
 	}
 
-	@Override
-	public int countByCompanyId(long companyId)
-		throws com.liferay.portal.kernel.exception.PortalException {
-
-		return _remoteAppEntryLocalService.countByCompanyId(companyId);
-	}
-
 	/**
 	 * @throws PortalException
 	 */
@@ -371,6 +364,13 @@ public class RemoteAppEntryLocalServiceWrapper
 	@Override
 	public int getRemoteAppEntriesCount() {
 		return _remoteAppEntryLocalService.getRemoteAppEntriesCount();
+	}
+
+	@Override
+	public int getRemoteAppEntriesCount(long companyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _remoteAppEntryLocalService.getRemoteAppEntriesCount(companyId);
 	}
 
 	/**
