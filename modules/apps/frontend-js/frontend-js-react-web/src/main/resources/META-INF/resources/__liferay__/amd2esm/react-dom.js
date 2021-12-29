ecd7b2ee07910d54a17be725941a72b1231862e9
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-12-29 14:53:58 +0100

    <d,e> LPS-143013 Fix react bridges in frontend-js-react-web
    We must export not only default, but all members. We will need to create
    an script to do this automatically during the build.

--- modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/__liferay__/amd2esm/react-dom.js
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/__liferay__/amd2esm/react-dom.js
@@ -1,7 +1,23 @@
-export default await new Promise((resolve, reject) => {
+const amdModule = await new Promise((resolve, reject) => {
 	Liferay.Loader.require(
 		'liferay!frontend-js-react-web$react-dom@16.12.0/index',
 		resolve,
 		reject
 	);
 });
+
+const {
+	createPortal, findDOMNode, flushSync, hydrate, render,
+	unmountComponentAtNode, unstable_batchedUpdates, unstable_createPortal,
+	unstable_renderSubtreeIntoContainer,
+	__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+} = amdModule;
+
+export {
+	createPortal, findDOMNode, flushSync, hydrate, render,
+	unmountComponentAtNode, unstable_batchedUpdates, unstable_createPortal,
+	unstable_renderSubtreeIntoContainer,
+	__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+};
+
+export default amdModule;
--- modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/__liferay__/amd2esm/react.js
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/__liferay__/amd2esm/react.js
@@ -1,7 +1,27 @@
-export default await new Promise((resolve, reject) => {
+const amdModule = await new Promise((resolve, reject) => {
 	Liferay.Loader.require(
 		'liferay!frontend-js-react-web$react@16.12.0/index',
 		resolve,
 		reject
 	);
 });
+
+const {
+	Children, Component, Fragment, Profiler, PureComponent, StrictMode,
+	Suspense, cloneElement, createContext, createElement, createFactory,
+	createRef, forwardRef, isValidElement, lazy, memo, useCallback, useContext,
+	useDebugValue, useEffect, useImperativeHandle, useLayoutEffect, useMemo,
+	useReducer, useRef, useState, version,
+	__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+} = amdModule;
+
+export {
+	Children, Component, Fragment, Profiler, PureComponent, StrictMode,
+	Suspense, cloneElement, createContext, createElement, createFactory,
+	createRef, forwardRef, isValidElement, lazy, memo, useCallback, useContext,
+	useDebugValue, useEffect, useImperativeHandle, useLayoutEffect, useMemo,
+	useReducer, useRef, useState, version,
+	__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED,
+};
+
+export default amdModule;
