608cf911a1cad65963310145827696ae594d861f
Iván Zaera Avellón <ivan.zaera@liferay.com>
2022-01-11 11:41:09 +0100

    LPS-142295 SF

--- modules/apps/remote-app/remote-app-service/src/main/java/com/liferay/remote/app/internal/upgrade/v2_4_0/SampleRemoteAppEntryUpgradeProcess.java
+++ modules/apps/remote-app/remote-app-service/src/main/java/com/liferay/remote/app/internal/upgrade/v2_4_0/SampleRemoteAppEntryUpgradeProcess.java
@@ -1,9 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.remote.app.internal.upgrade.v2_4_0;
 
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.remote.app.internal.instance.lifecycle.RemoteAppPortalInstanceLifecycleListener;
 
+/**
+ * @author Iván Zaera
+ */
 public class SampleRemoteAppEntryUpgradeProcess extends UpgradeProcess {
 
 	public SampleRemoteAppEntryUpgradeProcess(
@@ -23,6 +40,7 @@ public class SampleRemoteAppEntryUpgradeProcess extends UpgradeProcess {
 	}
 
 	private final CompanyLocalService _companyLocalService;
-	private final RemoteAppPortalInstanceLifecycleListener _companyModelListener;
+	private final RemoteAppPortalInstanceLifecycleListener
+		_companyModelListener;
 
 }
