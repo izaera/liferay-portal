6bf1acf6cf406f6e0a51007f06032a8cf68ebb73
Iván Zaera Avellón <ivan.zaera@liferay.com>
2022-03-24 16:19:27 +0100

    LPS-151395 Migrate frontend-js-web to ESM build

--- modules/npmscripts.config.js
+++ modules/npmscripts.config.js
@@ -382,6 +382,7 @@ module.exports = {
 				'react-dnd-html5-backend',
 				'react-dom',
 			],
+			'frontend-js-web': [],
 			'frontend-taglib-clay': [
 				'@clayui/alert',
 				'@clayui/autocomplete',
