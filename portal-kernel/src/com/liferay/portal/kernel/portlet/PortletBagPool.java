e1bb40ba0a3eea74c1410fc309ad7fd52b82b1c6
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-05-16 10:21:10 +0200

    LPS-183047 Scope portlet bags by company id
    Portlet bags hold the Java object implementing the portlets' render()
    method.
    
    Since we want to have different implementations for different companies
    under the same portletId, we need to scope portlet bags.
    
    Signed-off-by: Iván Zaera Avellón <ivan.zaera@liferay.com>

--- portal-kernel/src/com/liferay/portal/kernel/portlet/PortletBagPool.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/PortletBagPool.java
@@ -14,34 +14,33 @@
 
 package com.liferay.portal.kernel.portlet;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * @author Brian Wing Shun Chan
  */
 public class PortletBagPool {
 
-	public static PortletBag get(String portletId) {
-		return _portletBagPool.get(portletId);
+	public static PortletBag get(long companyId, String portletId) {
+		return _portletBagPoolMap.get(companyId, portletId);
 	}
 
-	public static void put(String portletId, PortletBag portletBag) {
-		_portletBagPool.put(portletId, portletBag);
+	public static void put(
+		long companyId, String portletId, PortletBag portletBag) {
+
+		_portletBagPoolMap.put(companyId, portletId, portletBag);
 	}
 
-	public static PortletBag remove(String portletId) {
-		return _portletBagPool.remove(portletId);
+	public static PortletBag remove(long companyId, String portletId) {
+		return _portletBagPoolMap.remove(companyId, portletId);
 	}
 
-	public static void reset() {
-		_portletBagPool.clear();
+	public static void reset(long companyId) {
+		_portletBagPoolMap.clear(companyId);
 	}
 
 	private PortletBagPool() {
 	}
 
-	private static final Map<String, PortletBag> _portletBagPool =
-		new ConcurrentHashMap<>();
+	private static final CompanyPortletMap<PortletBag> _portletBagPoolMap =
+		new CompanyPortletMap<>();
 
 }
