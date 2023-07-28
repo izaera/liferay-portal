90fce92e002b98ae2c9a02cec77a21d57c9e44d7
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-07-28 08:50:34 +0200

    LPS-178066 Ran ant build-taglibs

--- util-taglib/src/com/liferay/taglib/aui/base/BaseScriptTag.java
+++ util-taglib/src/com/liferay/taglib/aui/base/BaseScriptTag.java
@@ -16,9 +16,40 @@ import javax.servlet.jsp.JspException;
  */
 public abstract class BaseScriptTag extends com.liferay.taglib.util.PositionTagSupport {
 
-	@Override
 	public int doStartTag() throws JspException {
 		return super.doStartTag();
+	}
+
+	public boolean getAsync() {
+		return _async;
+	}
+
+	public java.lang.String getBlocking() {
+		return _blocking;
+	}
+
+	public java.lang.String getCrossorigin() {
+		return _crossorigin;
+	}
+
+	public boolean getDefer() {
+		return _defer;
+	}
+
+	public java.lang.String getFetchpriority() {
+		return _fetchpriority;
+	}
+
+	public java.lang.String getId() {
+		return _id;
+	}
+
+	public java.lang.String getIntegrity() {
+		return _integrity;
+	}
+
+	public java.lang.String getReferrerpolicy() {
+		return _referrerpolicy;
 	}
 
 	public java.lang.String getRequire() {
@@ -29,8 +60,52 @@ public abstract class BaseScriptTag extends com.liferay.taglib.util.PositionTagS
 		return _sandbox;
 	}
 
+	public java.lang.String getSenna() {
+		return _senna;
+	}
+
+	public java.lang.String getSrc() {
+		return _src;
+	}
+
+	public java.lang.String getType() {
+		return _type;
+	}
+
 	public java.lang.String getUse() {
 		return _use;
+	}
+
+	public void setAsync(boolean async) {
+		_async = async;
+	}
+
+	public void setBlocking(java.lang.String blocking) {
+		_blocking = blocking;
+	}
+
+	public void setCrossorigin(java.lang.String crossorigin) {
+		_crossorigin = crossorigin;
+	}
+
+	public void setDefer(boolean defer) {
+		_defer = defer;
+	}
+
+	public void setFetchpriority(java.lang.String fetchpriority) {
+		_fetchpriority = fetchpriority;
+	}
+
+	public void setId(java.lang.String id) {
+		_id = id;
+	}
+
+	public void setIntegrity(java.lang.String integrity) {
+		_integrity = integrity;
+	}
+
+	public void setReferrerpolicy(java.lang.String referrerpolicy) {
+		_referrerpolicy = referrerpolicy;
 	}
 
 	public void setRequire(java.lang.String require) {
@@ -41,16 +116,36 @@ public abstract class BaseScriptTag extends com.liferay.taglib.util.PositionTagS
 		_sandbox = sandbox;
 	}
 
+	public void setSenna(java.lang.String senna) {
+		_senna = senna;
+	}
+
+	public void setSrc(java.lang.String src) {
+		_src = src;
+	}
+
+	public void setType(java.lang.String type) {
+		_type = type;
+	}
+
 	public void setUse(java.lang.String use) {
 		_use = use;
 	}
 
-	@Override
 	protected void cleanUp() {
-		super.cleanUp();
-
+		_async = false;
+		_blocking = null;
+		_crossorigin = null;
+		_defer = false;
+		_fetchpriority = null;
+		_id = null;
+		_integrity = null;
+		_referrerpolicy = null;
 		_require = null;
 		_sandbox = false;
+		_senna = null;
+		_src = null;
+		_type = null;
 		_use = null;
 	}
 
@@ -61,8 +156,19 @@ public abstract class BaseScriptTag extends com.liferay.taglib.util.PositionTagS
 	private static final String _PAGE =
 		"/html/taglib/aui/script/page.jsp";
 
+	private boolean _async = false;
+	private java.lang.String _blocking = null;
+	private java.lang.String _crossorigin = null;
+	private boolean _defer = false;
+	private java.lang.String _fetchpriority = null;
+	private java.lang.String _id = null;
+	private java.lang.String _integrity = null;
+	private java.lang.String _referrerpolicy = null;
 	private java.lang.String _require = null;
 	private boolean _sandbox = false;
+	private java.lang.String _senna = null;
+	private java.lang.String _src = null;
+	private java.lang.String _type = null;
 	private java.lang.String _use = null;
 
 }
