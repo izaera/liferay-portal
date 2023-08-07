ebbe80158a909153257e99b6844c0a87753c2f17
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-08-07 14:16:34 +0200

    LPS-178066 Emit the CSP nonce from .tmpl templates

--- modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/product/navigation/control/menu/LayoutReportsProductNavigationControlMenuEntry.java
+++ modules/apps/layout/layout-reports-web/src/main/java/com/liferay/layout/reports/web/internal/product/navigation/control/menu/LayoutReportsProductNavigationControlMenuEntry.java
@@ -16,6 +16,7 @@ import com.liferay.layout.reports.web.internal.constants.ProductNavigationContro
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.content.security.policy.ContentSecurityPolicyNonceProviderUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.Language;
@@ -48,7 +49,6 @@ import com.liferay.taglib.util.BodyBottomTag;
 import java.io.IOException;
 import java.io.Writer;
 
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -113,39 +113,46 @@ public class LayoutReportsProductNavigationControlMenuEntry
 			HttpServletResponse httpServletResponse)
 		throws IOException {
 
-		Map<String, String> values = new HashMap<>();
-
-		if (isPanelStateOpen(
-				httpServletRequest,
-				ProductNavigationControlMenuEntryConstants.
-					SESSION_CLICKS_KEY)) {
-
-			values.put("cssClass", "active");
-		}
-		else {
-			values.put("cssClass", StringPool.BLANK);
-		}
-
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			_portal.getLocale(httpServletRequest), getClass());
-
-		values.put("title", _language.get(resourceBundle, "page-audit"));
-
-		IconTag iconTag = new IconTag();
-
-		iconTag.setCssClass("icon-monospaced");
-
-		if (FeatureFlagManagerUtil.isEnabled("LPS-187284")) {
-			iconTag.setSymbol("search-experiences");
-		}
-		else {
-			iconTag.setSymbol("info-circle");
-		}
+		Map<String, String> values;
 
 		try {
-			values.put(
+			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+				_portal.getLocale(httpServletRequest), getClass());
+
+			IconTag iconTag = new IconTag();
+
+			iconTag.setCssClass("icon-monospaced");
+
+			if (FeatureFlagManagerUtil.isEnabled("LPS-187284")) {
+				iconTag.setSymbol("search-experiences");
+			}
+			else {
+				iconTag.setSymbol("info-circle");
+			}
+
+			values = HashMapBuilder.put(
+				"cssClass",
+				() -> {
+					if (isPanelStateOpen(
+							httpServletRequest,
+							ProductNavigationControlMenuEntryConstants.
+								SESSION_CLICKS_KEY)) {
+
+						return "active";
+					}
+
+					return StringPool.BLANK;
+				}
+			).put(
 				"iconTag",
-				iconTag.doTagAsString(httpServletRequest, httpServletResponse));
+				iconTag.doTagAsString(httpServletRequest, httpServletResponse)
+			).put(
+				"nonceAttr",
+				ContentSecurityPolicyNonceProviderUtil.getNonceAttr(
+					httpServletRequest)
+			).put(
+				"title", _language.get(resourceBundle, "page-audit")
+			).build();
 		}
 		catch (JspException jspException) {
 			throw new IOException(jspException);
