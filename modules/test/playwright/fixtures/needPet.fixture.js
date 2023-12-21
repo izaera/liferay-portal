e21d6236a44c04de0541e895df30b3928da9252c
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-12-21 15:44:23 +0100

    Switch from per-test create/destroy of deps to pooling
    I'm pooling one single dog and cat among all tests. In this case I assume
    the pets are read-only so there's no problem in sharing them in several
    tests that are run in parallel.
    
    However, it seems very easy how we could create a mechanism to make tests
    relying on the same fixture dep be serialized automagically by the fixture
    or make them fail, or whatever we decide to do in that situation for any
    specific fixture dep type and name.

--- modules/test/playwright/fixtures/needPet.fixture.js
+++ modules/test/playwright/fixtures/needPet.fixture.js
@@ -7,23 +7,45 @@ import {test} from '@playwright/test';
 
 import {ApiHelpers} from '../helpers/ApiHelpers';
 
-let index = 0;
+let petPool = {
+	'dog': {
+		type: 'dog',
+		name: 'Tony',
+		i: 0
+	},
+	'cat': {
+		type: 'cat',
+		name: 'Garfield',
+		i: 0
+	}
+}
 
 exports.test = (petType) => 
 	test.extend({
 		_needPet: async ({page}, use) => {
-			// This 👇 simulates creating a pet in the DB 
-			let pet = {
-				type: petType,
-				name: petType == 'cat' ? 'Garfield' : 'Tony',
-				i: index++
-			};
+			// This 👇 simulates grabbing a precreated pet from the DB 
+			let pet = petPool[petType];
+
+			// Note that we could even create the pet before playwright is launched (pre-populated DB)
+			// and here we would only retrieve it from the DB once per test run.
+			//
+			// Alternatively we could insert it here on the fly. The possibilities are infinite because
+			// we are offloading the strategy to this file, instead of leaving it inside the test.
 
 			try {
 				await use(pet);
 			}
 			finally {
-				// Here we would clean up the pet from the DB
+				// Here we would free up the pet from the pool (if necessary)
+
+				// Note that for read-only objects we may simply share them across tests. For read-write
+				// we would need to make sure we don't reuse the same object from the pool for two tests
+				// but I believe we can do it easily since  the call to `use()` (which is what makes the
+				// test run) is asynchronous, thus we can hold a test execution from the fixture for as 
+				// long as we want until the pooled object is ready.
+				
+				// BTW, this can be used to synchronize tests too (think of tests that declare a 
+				// dependency on a certain fixture and then that fixture makes them run serially) 😱.				
 			}
 		},
 	});
