dba9a8a133eceb785fb18ec2bdffa88a6e302ef9
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-08-06 09:16:06 +0200

    LPD-19791 Replace raw <style> with <aui:style> in Java classes
    So that the CSP nonce is added.

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentEntryFragmentRenderer.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentEntryFragmentRenderer.java
@@ -278,7 +278,11 @@ public class FragmentEntryFragmentRenderer implements FragmentRenderer {
 			if (fragmentRendererContext.isEditMode() ||
 				fragmentRendererContext.isIndexMode()) {
 
-				sb.append("<style>");
+				sb.append("<style");
+				sb.append(
+					ContentSecurityPolicyNonceProviderUtil.getNonceAttribute(
+						httpServletRequest));
+				sb.append(StringPool.GREATER_THAN);
 				sb.append(css);
 				sb.append("</style>");
 			}
@@ -309,7 +313,11 @@ public class FragmentEntryFragmentRenderer implements FragmentRenderer {
 				}
 
 				if (!cssLoaded) {
-					sb.append("<style>");
+					sb.append("<style");
+					sb.append(
+						ContentSecurityPolicyNonceProviderUtil.getNonceAttribute(
+							httpServletRequest));
+					sb.append(StringPool.GREATER_THAN);
 					sb.append(css);
 					sb.append("</style>");
 
