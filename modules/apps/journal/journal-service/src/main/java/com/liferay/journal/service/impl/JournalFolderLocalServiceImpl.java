2b1e71aff55b9122da1908d7fa3f6914171c0957
Akhash R <akhash.ramprakash@liferay.com>
2025-04-01 14:29:45 +0530

    LPD-52287 update status on Add and Update

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalFolderLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -123,6 +123,9 @@ public class JournalFolderLocalServiceImpl
 		folder.setTreePath(folder.buildTreePath());
 		folder.setName(name);
 		folder.setDescription(description);
+		folder.setStatusByUserId(user.getUserId());
+		folder.setStatusByUserName(user.getFullName());
+		folder.setStatusDate(serviceContext.getModifiedDate(new Date()));
 		folder.setExpandoBridgeAttributes(serviceContext);
 
 		folder = journalFolderPersistence.update(folder);
@@ -1446,6 +1449,8 @@ public class JournalFolderLocalServiceImpl
 
 		_validateFolder(folderId, folder.getGroupId(), parentFolderId, name);
 
+		User user = _userLocalService.getUser(userId);
+
 		long oldParentFolderId = folder.getParentFolderId();
 
 		if (oldParentFolderId != parentFolderId) {
@@ -1456,6 +1461,9 @@ public class JournalFolderLocalServiceImpl
 		folder.setName(name);
 		folder.setDescription(description);
 		folder.setRestrictionType(restrictionType);
+		folder.setStatusByUserId(user.getUserId());
+		folder.setStatusByUserName(user.getFullName());
+		folder.setStatusDate(serviceContext.getModifiedDate(new Date()));
 		folder.setExpandoBridgeAttributes(serviceContext);
 
 		folder = journalFolderPersistence.update(folder);
