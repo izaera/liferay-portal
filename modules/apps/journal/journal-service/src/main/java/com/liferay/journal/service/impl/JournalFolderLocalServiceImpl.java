1701c182612f5e7ec741f557010d9568cd22cefe
Brian Chan <brian.chan@liferay.com>
2025-04-03 12:02:24 +0200

    LPD-52287 Pattern in this class (vs. as used)

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalFolderLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalFolderLocalServiceImpl.java
@@ -1411,6 +1411,8 @@ public class JournalFolderLocalServiceImpl
 
 		// Merge folders
 
+		User user = _userLocalService.getUser(userId);
+
 		if ((restrictionType !=
 				JournalFolderConstants.
 					RESTRICTION_TYPE_DDM_STRUCTURES_AND_WORKFLOW) &&
@@ -1448,8 +1450,6 @@ public class JournalFolderLocalServiceImpl
 		// Folder
 
 		_validateFolder(folderId, folder.getGroupId(), parentFolderId, name);
-
-		User user = _userLocalService.getUser(userId);
 
 		long oldParentFolderId = folder.getParentFolderId();
 
