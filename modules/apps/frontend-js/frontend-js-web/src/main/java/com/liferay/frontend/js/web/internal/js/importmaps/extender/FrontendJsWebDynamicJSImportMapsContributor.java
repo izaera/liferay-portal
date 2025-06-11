0209ecbe282ccd37f3b5c609299a4d68679589cb
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-06-11 15:11:50 +0200

    LPD-52709 Add a frontend resource request handler for I18N labels modules

--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/js/importmaps/extender/FrontendJsWebDynamicJSImportMapsContributor.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/js/importmaps/extender/FrontendJsWebDynamicJSImportMapsContributor.java
@@ -6,6 +6,7 @@
 package com.liferay.frontend.js.web.internal.js.importmaps.extender;
 
 import com.liferay.frontend.js.importmaps.extender.DynamicJSImportMapsContributor;
+import com.liferay.frontend.js.web.internal.resource.handler.LanguageFrontendResourceRequestHandler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesRegistry;
 import com.liferay.portal.kernel.util.Portal;
@@ -30,7 +31,13 @@ public class FrontendJsWebDynamicJSImportMapsContributor
 			HttpServletRequest httpServletRequest, Writer writer)
 		throws IOException {
 
-		writer.write("\"@liferay/language/\": \"/o/js/language/\"");
+		writer.write(StringPool.QUOTE);
+		writer.write(
+			LanguageFrontendResourceRequestHandler.LANGUAGE_MODULE_PREFIX);
+		writer.write("\": \"");
+		writer.write(
+			LanguageFrontendResourceRequestHandler.LANGUAGE_URI_PREFIX);
+		writer.write(StringPool.QUOTE);
 
 		_hashedFilesRegistry.forEach(
 			(unhashedFileURI, hashedFileURI) -> {
