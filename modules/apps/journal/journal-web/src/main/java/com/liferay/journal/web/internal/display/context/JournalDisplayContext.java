0345fabc2f954f4572f76be3103728ed5c909fc7
Mikel Lorza <mikel.lorza@liferay.com>
2025-07-16 10:55:06 +0200

    LPD-60893 Add isSearch since we are showing messages only when we are searching, so, if there is no search we do not have to show messages
    (cherry picked from commit 9e85d524ea284586ba7542b1626f3546a11e8dbe)

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1326,7 +1326,7 @@ public class JournalDisplayContext {
 		}
 
 		if (((isTypeWebContent() && !hasResults()) || isTypeVersions()) &&
-			!hasVersionsResults() && hasCommentsResults()) {
+			!hasVersionsResults() && hasCommentsResults() && isSearch()) {
 
 			_searchIn = "comments";
 
