a72232c1809a033f1cf06cb8881ffdf32f8b22a6
Minhchau <minhchau.dang@liferay.com>
2025-07-22 17:36:21 +0900

    LPD-59693 Introduce disambiguations for "to", which can refer to a datetime range, a pagination range, a translation target language, or an email recipient
    * For datetime ranges, to[datetime] has been added
    * For pagination ranges, frontend-taglib-clay was the only instance of that, and it has been updated to use showing-x-x-of-x-results which was intended for that disambiguation
    * For translation target languages, translation-web was the only instance of that, and while we have a noun language key for this purpose (translation-of-x-to-x), the UI refers to a verb, and so it has been left alone
    * For email recipients, to[recipient] has been added

--- modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/data_set/filters/components/DateRangeFilter.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/data_set/filters/components/DateRangeFilter.tsx
@@ -271,7 +271,7 @@ function Body({
 
 						<div className="form-group-item">
 							<label htmlFor={toFormElementId}>
-								{Liferay.Language.get('to')}
+								{Liferay.Language.get('to[datetime]')}
 							</label>
 
 							<ClayDatePicker
