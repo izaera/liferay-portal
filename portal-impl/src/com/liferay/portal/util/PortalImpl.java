7fc06843723513f214f4b83fc3d9d042e2c86363
Brian Chan <brian.chan@liferay.com>
2025-08-05 08:55:13 +0800

    LPD-62227 Set only when needed
    (cherry picked from commit 62b62cc4b56d810a8f08cb90af022d27092b25d0)

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1098,10 +1098,10 @@ public class PortalImpl implements Portal {
 
 		Layout layout = null;
 
-		HttpServletRequest httpServletRequest =
-			(HttpServletRequest)requestContext.get("request");
-
 		if (Validator.isNull(friendlyURL)) {
+			HttpServletRequest httpServletRequest =
+				(HttpServletRequest)requestContext.get("request");
+
 			if (AuthLoginGroupSettingsUtil.isPromptEnabled(groupId) &&
 				!_isSignedIn(httpServletRequest) &&
 				!GetterUtil.getBoolean(
