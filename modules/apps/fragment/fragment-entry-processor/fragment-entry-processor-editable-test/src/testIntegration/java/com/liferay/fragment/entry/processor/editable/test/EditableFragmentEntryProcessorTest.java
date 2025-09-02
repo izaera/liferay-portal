f10c66d5e683733950cbc59d4e6a613acd936e56
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-09-02 18:53:59 +0200

    LPD-63798 See LPD-64268: until LayoutServiceContextHelperImpl is fixed to restore the original serviceContext properly by using pop, we need to set the portalURL so all themeDisplay portalURLs match and the asserts work

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/EditableFragmentEntryProcessorTest.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/EditableFragmentEntryProcessorTest.java
@@ -1809,6 +1809,7 @@ public class EditableFragmentEntryProcessorTest {
 			_themeLocalService.getTheme(
 				_company.getCompanyId(), layoutSet.getThemeId()),
 			null);
+		themeDisplay.setPortalURL(_company.getPortalURL(_group.getGroupId()));
 		themeDisplay.setRealUser(TestPropsValues.getUser());
 		themeDisplay.setRequest(_getHttpServletRequest(locale));
 		themeDisplay.setResponse(new MockHttpServletResponse());
