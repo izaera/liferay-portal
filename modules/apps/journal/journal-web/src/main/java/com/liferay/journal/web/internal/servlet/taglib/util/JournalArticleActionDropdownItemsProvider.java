89cb06ffe87b16e714f433cadb029214cf70b61e
Brian Chan <brian.chan@liferay.com>
2025-09-23 12:02:26 -0700

    LPD-65083 Rename
    (cherry picked from commit d2c6ce208a30875c3028fb70a3bd036829db9f14)

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/util/JournalArticleActionDropdownItemsProvider.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/util/JournalArticleActionDropdownItemsProvider.java
@@ -312,7 +312,7 @@ public class JournalArticleActionDropdownItemsProvider {
 			dropdownGroupItem -> {
 				dropdownGroupItem.setDropdownItems(
 					DropdownItemListBuilder.add(
-						this::_isDeleteActionAvailable,
+						this::_hasDeleteArticleAction,
 						_getDeleteArticleAction(
 							articleId, _themeDisplay.getURLCurrent())
 					).build());
@@ -1062,7 +1062,7 @@ public class JournalArticleActionDropdownItemsProvider {
 		return false;
 	}
 
-	private boolean _isDeleteActionAvailable() throws PortalException {
+	private boolean _hasDeleteArticleAction() throws PortalException {
 		if (!JournalArticlePermission.contains(
 				_themeDisplay.getPermissionChecker(), _article,
 				ActionKeys.DELETE)) {
