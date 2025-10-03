7221e9005fc2390d7e26c32fd5d2ea4b9b2929b2
IstvanD <istvan.dezsi@liferay.com>
2025-10-03 14:02:55 +0200

    LPD-67395 AssetCategoryTree is not firing selection event when all categories are cleared

--- modules/apps/asset/asset-categories-item-selector-web/src/main/resources/META-INF/resources/js/AssetCategoryTree.js
+++ modules/apps/asset/asset-categories-item-selector-web/src/main/resources/META-INF/resources/js/AssetCategoryTree.js
@@ -93,11 +93,9 @@ export function AssetCategoryTree({
 		}
 
 		requestAnimationFrame(() => {
-			if (Object.keys(selectedItems).length) {
-				getOpener().Liferay.fire(itemSelectedEventName, {
-					data: selectedItems,
-				});
-			}
+			getOpener().Liferay.fire(itemSelectedEventName, {
+				data: selectedItems,
+			});
 		});
 	}, [
 		selectedKeys,
