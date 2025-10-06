7527f86b2f88d068471b12edad7325a7fdd1c52f
Chaitanya Sammetla <chaitanya.sammetla@liferay.com>
2025-10-06 14:02:14 +0530

    LPD-66338 Update test

--- modules/apps/object/object-web/src/test/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImplTest.java
+++ modules/apps/object/object-web/src/test/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImplTest.java
@@ -32,10 +32,10 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 import java.util.Collections;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
