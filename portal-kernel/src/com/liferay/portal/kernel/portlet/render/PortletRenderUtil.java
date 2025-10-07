d4b303c9d03937fea67b88bbe85f5d6697882ae2
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-10-07 15:56:11 +0200

    LPD-52709 Remove dead code

--- portal-kernel/src/com/liferay/portal/kernel/portlet/render/PortletRenderUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/render/PortletRenderUtil.java
@@ -601,7 +601,7 @@ public class PortletRenderUtil {
 
 		if (javaScriptPath.startsWith("module:")) {
 			javaScriptPath = javaScriptPath.substring(7);
-			type = FrontendESMUtil.getScriptType();
+			type = "module";
 		}
 
 		printWriter.print("<script");
