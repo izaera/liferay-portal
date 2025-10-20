ff61e12b48d4d76a91a65279a3034fe4bbc7e553
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-10-20 12:26:50 +0200

    LPD-67267 TypeScript integration
    This is a way to tell TypeScript where to get the types for CKEditor split
    packages.
    
    Another option would be using bare identifiers in source code (for example:
    `@ckeditor/ckeditor5-core` instead of
    `@ckeditor/ckeditor5-core/dist/index.js`, then configuring aliases to
    rewrite imports from `@ckeditor/ckeditor5-core` to
    `@ckeditor/ckeditor5-core/dist/index.js`.
    
    It seems to me that the `types.d.ts` option is easier to maintain and also
    clearer since what you see in code is what you get at runtime.
    
    In the end, it's a TypeScript problem that has nothing to do with code and
    bundling so it wouldn't be fair to affect those two.

---
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-sample-web/src/main/resources/META-INF/resources/js/types.d.ts
@@ -0,0 +1,6 @@
+declare module '@ckeditor/ckeditor5-core/dist/index.js' {
+    export * from '@ckeditor/ckeditor5-core';
+}
+declare module '@ckeditor/ckeditor5-ui/dist/index.js' {
+    export * from '@ckeditor/ckeditor5-ui';
+}
---
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-editor-config-contributor/src/types.d.ts
@@ -0,0 +1,9 @@
+declare module '@ckeditor/ckeditor5-core/dist/index.js' {
+    export * from '@ckeditor/ckeditor5-core';
+}
+declare module '@ckeditor/ckeditor5-fullscreen/dist/index.js' {
+    export * from '@ckeditor/ckeditor5-fullscreen';
+}
+declare module '@ckeditor/ckeditor5-ui/dist/index.js' {
+    export * from '@ckeditor/ckeditor5-ui';
+}
