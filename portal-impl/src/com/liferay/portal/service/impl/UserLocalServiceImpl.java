774576fbf2277695024867d29609e2d2c509908f
Eric Yan <eric.yan@liferay.com>
2025-10-22 14:51:28 -0700

    LPD-69401 Refactor, no logic change; prep next

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1523,18 +1523,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 					TransactionInvokerUtil.invoke(
 						_transactionConfig,
 						() -> {
-							Session session = null;
-
-							try {
-								session = userPersistence.openSession();
-
-								session.apply(
-									connection -> _updateLastLogin(
-										connection, deduplicatedUsers));
-							}
-							finally {
-								userPersistence.closeSession(session);
-							}
+							_updateLastLogin(deduplicatedUsers);
 
 							return null;
 						});
@@ -7647,6 +7636,19 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 						UserImpl.class, user.getUserId());
 				}
 			}
+		}
+	}
+
+	private void _updateLastLogin(List<User> users) throws SQLException {
+		Session session = null;
+
+		try {
+			session = userPersistence.openSession();
+
+			session.apply(connection -> _updateLastLogin(connection, users));
+		}
+		finally {
+			userPersistence.closeSession(session);
 		}
 	}
 
