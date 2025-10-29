c29a5ff12dd9bafb0fb429f993d57b54478442c4
Victor Kammerer <vko@cin.ufpe.com>
2025-10-29 10:16:05 -0300

    LPD-65635 build-service-portal

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -514,4 +514,5 @@ create unique index IX_BF1277A7 on WorkflowDefinitionLink (groupId, ctCollection
 create unique index IX_6D62D29A on WorkflowDefinitionLink (groupId, uuid_[$COLUMN_LENGTH:75$], ctCollectionId);
 create index IX_336A4F02 on WorkflowDefinitionLink (uuid_[$COLUMN_LENGTH:75$]);
 
-create index IX_415A7007 on WorkflowInstanceLink (groupId, companyId, classNameId, classPK);
+create index IX_AC22B613 on WorkflowInstanceLink (companyId, classNameId, groupId, classPK);
+create unique index IX_BA3974B5 on WorkflowInstanceLink (workflowInstanceId, ctCollectionId);
