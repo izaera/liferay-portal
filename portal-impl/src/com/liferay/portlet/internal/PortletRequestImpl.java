438070531fd6a2cb016141ad9b3658ead265cace
fortunato <fortunato.maldonado@liferay.com>
2025-10-30 12:21:07 -0600

    LPD-69950 Prevent ExpandoAttribute to be processed

--- portal-impl/src/com/liferay/portlet/internal/PortletRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/PortletRequestImpl.java
@@ -38,6 +38,7 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.servlet.NamespaceServletRequest;
@@ -812,6 +813,8 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 			dynamicServletRequest, publicRenderParametersMap,
 			portletPreferences, getLifecycle());
 
+		_processCheckbox(dynamicServletRequest);
+
 		if (!isPortletModeAllowed(portletMode)) {
 			portletMode = PortletModeFactory.getPortletMode(null, 3);
 
@@ -1225,6 +1228,30 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 			}
 			else {
 				dynamicServletRequest.setParameterValues(name, newValues);
+			}
+		}
+	}
+
+	private void _processCheckbox(DynamicServletRequest dynamicServletRequest) {
+		String checkboxNames = dynamicServletRequest.getParameter(
+			"checkboxNames");
+
+		if (Validator.isNull(checkboxNames)) {
+			return;
+		}
+
+		for (String checkboxName : StringUtil.split(checkboxNames)) {
+			String value = dynamicServletRequest.getParameter(checkboxName);
+
+			if (!checkboxName.contains("ExpandoAttribute")) {
+				if (value == null) {
+					dynamicServletRequest.setParameter(
+						checkboxName, Boolean.FALSE.toString());
+				}
+				else if (Objects.equals(value, "on")) {
+					dynamicServletRequest.setParameter(
+						checkboxName, Boolean.TRUE.toString());
+				}
 			}
 		}
 	}
