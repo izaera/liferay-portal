bac66003dcf7c08523d31f69bc5a71fd38160870
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-10-30 11:30:17 +0100

    LPD-70097 Include tests
    (cherry picked from commit 1f8dbe51e48ec7044156283ad71cf64484d5c567)

--- modules/apps/document-library/document-library-preview-document/test/DocumentPreviewer.es.js
+++ modules/apps/document-library/document-library-preview-document/test/DocumentPreviewer.es.js
@@ -3,8 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {cleanup, render} from '@testing-library/react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
 
 import {DocumentPreviewer} from '../src/main/resources/META-INF/resources/preview/js/index';
 
@@ -49,5 +51,28 @@ describe('document-library-preview-document', () => {
 		);
 
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('can change current page with keyboard', () => {
+		const {getByPlaceholderText, getByRole, getByText} = render(
+			<DocumentPreviewer
+				alt="alt text"
+				baseImageURL="http://localhost/document-images/"
+				initialPage={1}
+				totalPages={10}
+			/>
+		);
+
+		const button = getByRole('button', {
+			name: /page\s*1\s*\/\s*10/i,
+		});
+		fireEvent.click(button);
+
+		const input = getByPlaceholderText('page-...');
+
+		fireEvent.change(input, {target: {value: '3'}});
+		fireEvent.keyDown(input, {key: 'Enter'});
+
+		expect(getByText('page 3 / 10')).toBeInTheDocument();
 	});
 });
