264bcc4fac6ba7de23fda84a9b75115cb84648c2
Roberto Díaz <roberto.diaz@liferay.com>
2025-11-04 14:45:08 +0100

    LPD-70535 Refactor `ContentFieldValue` file entry retrieval logic for improved null handling and ID resolution

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/ContentFieldUtil.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/dto/v1_0/util/ContentFieldUtil.java
@@ -323,9 +323,9 @@ public class ContentFieldUtil {
 				JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
 					valueString);
 
-				long fileEntryId = jsonObject.getLong("fileEntryId");
+				FileEntry fileEntry = _getFileEntry(dlAppService, valueString);
 
-				if (fileEntryId == 0) {
+				if (fileEntry == null) {
 					return new ContentFieldValue();
 				}
 
@@ -333,8 +333,9 @@ public class ContentFieldUtil {
 					{
 						setImage(
 							() -> _toImage(
-								dlURLHelper, dlAppService, fileEntryId, uriInfo,
-								jsonObject, locale));
+								dlURLHelper, dlAppService,
+								fileEntry.getFileEntryId(), uriInfo, jsonObject,
+								locale));
 					}
 				};
 			}
@@ -543,6 +544,18 @@ public class ContentFieldUtil {
 
 		if (classPK != 0) {
 			return dlAppService.getFileEntry(classPK);
+		}
+
+		long fileEntryId = jsonObject.getLong("fileEntryId");
+
+		if (fileEntryId != 0) {
+			return dlAppService.getFileEntry(fileEntryId);
+		}
+
+		long id = jsonObject.getLong("id");
+
+		if (id != 0) {
+			return dlAppService.getFileEntry(id);
 		}
 
 		long groupId = jsonObject.getLong("groupId");
