ad8785e783972f3882a1b7699e07c2116ac8e2e2
Noor Najjar <noor.najjar@liferay.com>
2025-11-12 11:25:59 -0800

    LPD-71160 Add tests

--- modules/apps/change-tracking/change-tracking-rest-test/src/testIntegration/java/com/liferay/change/tracking/rest/resource/v1_0/test/CTProcessResourceTest.java
+++ modules/apps/change-tracking/change-tracking-rest-test/src/testIntegration/java/com/liferay/change/tracking/rest/resource/v1_0/test/CTProcessResourceTest.java
@@ -8,6 +8,8 @@ package com.liferay.change.tracking.rest.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.rest.client.dto.v1_0.CTProcess;
+import com.liferay.change.tracking.rest.client.pagination.Page;
+import com.liferay.change.tracking.rest.client.pagination.Pagination;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.change.tracking.service.CTProcessLocalService;
 import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
@@ -20,6 +22,9 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -28,6 +33,25 @@ import org.junit.runner.RunWith;
 @DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class CTProcessResourceTest extends BaseCTProcessResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetCTProcessesPage() throws Exception {
+		super.testGetCTProcessesPage();
+
+		_addCTProcess();
+		_addCTProcess();
+
+		Page<CTProcess> page = ctProcessResource.getCTProcessesPage(
+			null, null, null, Pagination.of(1, 10), null);
+
+		Page<CTProcess> descPage = ctProcessResource.getCTProcessesPage(
+			null, null, null, Pagination.of(1, 10), "datePublished:desc");
+
+		assertEquals(
+			(List<CTProcess>)descPage.getItems(),
+			(List<CTProcess>)page.getItems());
+	}
 
 	@Override
 	protected String[] getAdditionalAssertFieldNames() {
