1edc4af26cee138bccba1b8125fb393bd3fc576c
fortunato <fortunato.maldonado@liferay.com>
2025-11-18 12:48:11 -0700

    LPD-70011 Revert "LPD-70011 - Fix unit tests"
    This reverts commit 591075ee1c131a6feb0c03a1b89c80f5c8870b6e.

--- modules/_node-scripts/test/jest/setupAfterEnv.js
+++ modules/_node-scripts/test/jest/setupAfterEnv.js
@@ -9,11 +9,6 @@ afterEach(() => {
 	global.fetch.mockRestore();
 });
 
-beforeAll(() => {
-	global.TextEncoder = require('util').TextEncoder;
-	global.TextDecoder = require('util').TextDecoder;
-});
-
 beforeEach(() => {
 	jest.spyOn(global, 'fetch').mockImplementation((...args) => {
 		throw new Error(
