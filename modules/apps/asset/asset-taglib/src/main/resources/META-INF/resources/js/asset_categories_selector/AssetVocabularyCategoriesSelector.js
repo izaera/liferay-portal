50572dc066d4a181b1ffec2ed0355178d692872d
Alicia García <alicia.garcia@liferay.com>
2025-11-21 11:27:28 +0100

    LPD-72245 add clearAllTitle to ClayMultiSelect to translate the clear all title for tags and categories selector

--- modules/apps/asset/asset-taglib/src/main/resources/META-INF/resources/js/asset_categories_selector/AssetVocabularyCategoriesSelector.js
+++ modules/apps/asset/asset-taglib/src/main/resources/META-INF/resources/js/asset_categories_selector/AssetVocabularyCategoriesSelector.js
@@ -229,6 +229,7 @@ function AssetVocabulariesCategoriesSelector({
 				<ClayInput.Group>
 					<ClayInput.GroupItem>
 						<ClayMultiSelect
+							clearAllTitle={Liferay.Language.get('clear-all')}
 							id={inputName + '_MultiSelect'}
 							inputName={inputName}
 							items={selectedItems}
