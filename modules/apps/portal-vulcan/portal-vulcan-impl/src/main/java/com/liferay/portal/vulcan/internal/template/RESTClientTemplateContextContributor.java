aee36de9116e387f821f1b8e9045517408f7b904
GaborKomaromi <gabor.komaromi@liferay.com>
2025-11-26 13:59:03 +0100

    LPD-71461 Revert "LPD-66204 Returning an instance of ServletRequestWrapper as per spec"
    This reverts commit ac72705aa91bd483b76ae992c5eb70caa87b57fd.

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/template/RESTClientTemplateContextContributor.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/template/RESTClientTemplateContextContributor.java
@@ -28,9 +28,7 @@ import java.util.Objects;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
@@ -97,20 +95,12 @@ public class RESTClientTemplateContextContributor
 				AccessControlUtil.setAccessControlContext(null);
 
 				requestDispatcher.forward(
-					new HttpServletRequestWrapper(
-						ProxyUtil.newDelegateProxyInstance(
-							HttpServletRequest.class.getClassLoader(),
-							HttpServletRequest.class,
-							new RESTClientHttpRequestDelegate(
-								_contextObjects, _httpServletRequest, path),
-							_httpServletRequest)) {
-
-						@Override
-						public ServletRequest getRequest() {
-							return _httpServletRequest;
-						}
-
-					},
+					ProxyUtil.newDelegateProxyInstance(
+						HttpServletRequest.class.getClassLoader(),
+						HttpServletRequest.class,
+						new RESTClientHttpRequestDelegate(
+							_contextObjects, _httpServletRequest, path),
+						_httpServletRequest),
 					httpServletResponse);
 			}
 			finally {
