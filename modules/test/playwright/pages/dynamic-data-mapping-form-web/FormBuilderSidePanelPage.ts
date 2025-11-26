3e15dbd0fd2d523e1cf7b00f15e168e210e4832e
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2025-11-26 19:00:25 -0300

    LPD-72655 Create test to assert that removing decimal separator does not accidentaly remove characters as well

--- modules/test/playwright/pages/dynamic-data-mapping-form-web/FormBuilderSidePanelPage.ts
+++ modules/test/playwright/pages/dynamic-data-mapping-form-web/FormBuilderSidePanelPage.ts
@@ -18,7 +18,9 @@ export class FormBuilderSidePanelPage {
 	readonly backButton: Locator;
 	readonly helpText: Locator;
 	readonly htmlAutocompleteAttributeField: Locator;
+	readonly inputMaskToggle: Locator;
 	readonly label: Locator;
+	readonly numericTypeDecimal: Locator;
 	readonly objectFieldSelect: Locator;
 	readonly page: Page;
 	readonly paragraphFieldTextarea: Locator;
@@ -55,7 +57,9 @@ export class FormBuilderSidePanelPage {
 		this.htmlAutocompleteAttributeField = page.getByLabel(
 			'HTML Autocomplete Attribute'
 		);
+		this.inputMaskToggle = page.getByLabel('Input Mask');
 		this.label = page.getByLabel('Label', {exact: true}).first();
+		this.numericTypeDecimal = page.getByLabel('Decimal', {exact: true});
 		this.objectFieldSelect = page.getByLabel('Object Field');
 		this.page = page;
 		this.paragraphFieldTextarea = page
