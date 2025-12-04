78064d0bfcc576d92dc7541bf47b29d4541ccf0f
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-12-04 13:46:30 +0100

    LPD-71877 Is always the same plid

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtil.java
@@ -203,8 +203,7 @@ public class SegmentsExperienceUtil {
 	}
 
 	private static void _copyPortletPermissions(
-		long companyId, String newNamespace, String portletId,
-		long sourcePlid, long targetPlid) {
+		long companyId, String newNamespace, String portletId, long plid) {
 
 		String targetPortletId = _getNewPortletId(newNamespace, portletId);
 
@@ -212,10 +211,10 @@ public class SegmentsExperienceUtil {
 			portletId);
 
 		String sourcePrimKey = PortletPermissionUtil.getPrimaryKey(
-			sourcePlid, portletId);
+			plid, portletId);
 
 		String targetPrimKey = PortletPermissionUtil.getPrimaryKey(
-			targetPlid, targetPortletId);
+			plid, targetPortletId);
 
 		List<ResourcePermission> resourcePermissions =
 			ResourcePermissionLocalServiceUtil.getResourcePermissions(
@@ -257,8 +256,7 @@ public class SegmentsExperienceUtil {
 
 			_copyPortletPermissions(
 				fragmentEntryLink.getCompanyId(),
-				newFragmentEntryLink.getNamespace(), portletId,
-				fragmentEntryLink.getPlid(), newFragmentEntryLink.getPlid());
+				newFragmentEntryLink.getNamespace(), portletId, plid);
 		}
 	}
 
