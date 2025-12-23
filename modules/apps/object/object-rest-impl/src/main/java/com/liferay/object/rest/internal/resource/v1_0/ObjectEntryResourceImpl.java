ded7f3041de90a838526b199684d2ef7aaa407fc
Brian Chan <brian.chan@liferay.com>
2025-12-23 13:22:53 +0530

    LPD-74694 SF

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectEntryResourceImpl.java
@@ -564,6 +564,61 @@ public class ObjectEntryResourceImpl extends BaseObjectEntryResourceImpl {
 		}
 	}
 
+	private void _create(
+			Collection<ObjectEntry> objectEntries,
+			Map<String, Serializable> parameters)
+		throws Exception {
+
+		String createStrategy = (String)parameters.getOrDefault(
+			"createStrategy", "INSERT");
+		String scopeKey = _getScopeKey(parameters);
+		UnsafeFunction<ObjectEntry, ObjectEntry, Exception> unsafeFunction =
+			null;
+
+		if (StringUtil.equalsIgnoreCase(createStrategy, "INSERT")) {
+			unsafeFunction = objectEntry -> postScopeScopeKey(
+				scopeKey, objectEntry);
+		}
+		else if (StringUtil.equalsIgnoreCase(createStrategy, "UPSERT")) {
+			String updateStrategy = (String)parameters.getOrDefault(
+				"updateStrategy", "UPDATE");
+
+			if (StringUtil.equalsIgnoreCase(updateStrategy, "PARTIAL_UPDATE")) {
+				unsafeFunction = objectEntry -> {
+					try {
+						ObjectEntry getObjectEntry =
+							getScopeScopeKeyByExternalReferenceCode(
+								scopeKey,
+								objectEntry.getExternalReferenceCode());
+
+						return patchObjectEntry(
+							getObjectEntry.getId(), objectEntry);
+					}
+					catch (NoSuchModelException noSuchModelException) {
+						if (_log.isDebugEnabled()) {
+							_log.debug(noSuchModelException);
+						}
+
+						return postScopeScopeKey(scopeKey, objectEntry);
+					}
+				};
+			}
+			else if (StringUtil.equalsIgnoreCase(updateStrategy, "UPDATE")) {
+				unsafeFunction =
+					objectEntry -> putScopeScopeKeyByExternalReferenceCode(
+						scopeKey, objectEntry.getExternalReferenceCode(),
+						objectEntry);
+			}
+		}
+
+		if (unsafeFunction == null) {
+			throw new NotSupportedException(
+				"Create strategy \"" + createStrategy + "\" is not supported");
+		}
+
+		contextBatchUnsafeBiConsumer.accept(objectEntries, unsafeFunction);
+	}
+
 	private DefaultDTOConverterContext _getDTOConverterContext(
 		Long objectEntryId) {
 
@@ -660,62 +715,6 @@ public class ObjectEntryResourceImpl extends BaseObjectEntryResourceImpl {
 		}
 
 		return null;
-	}
-
-	private void _create(
-			Collection<ObjectEntry> objectEntries,
-			Map<String, Serializable> parameters)
-		throws Exception {
-
-		String scopeKey = _getScopeKey(parameters);
-		UnsafeFunction<ObjectEntry, ObjectEntry, Exception> unsafeFunction =
-			null;
-
-		String createStrategy = (String)parameters.getOrDefault(
-			"createStrategy", "INSERT");
-
-		if (StringUtil.equalsIgnoreCase(createStrategy, "INSERT")) {
-			unsafeFunction = objectEntry -> postScopeScopeKey(
-				scopeKey, objectEntry);
-		}
-		else if (StringUtil.equalsIgnoreCase(createStrategy, "UPSERT")) {
-			String updateStrategy = (String)parameters.getOrDefault(
-				"updateStrategy", "UPDATE");
-
-			if (StringUtil.equalsIgnoreCase(updateStrategy, "PARTIAL_UPDATE")) {
-				unsafeFunction = objectEntry -> {
-					try {
-						ObjectEntry getObjectEntry =
-							getScopeScopeKeyByExternalReferenceCode(
-								scopeKey,
-								objectEntry.getExternalReferenceCode());
-
-						return patchObjectEntry(
-							getObjectEntry.getId(), objectEntry);
-					}
-					catch (NoSuchModelException noSuchModelException) {
-						if (_log.isDebugEnabled()) {
-							_log.debug(noSuchModelException);
-						}
-
-						return postScopeScopeKey(scopeKey, objectEntry);
-					}
-				};
-			}
-			else if (StringUtil.equalsIgnoreCase(updateStrategy, "UPDATE")) {
-				unsafeFunction =
-					objectEntry -> putScopeScopeKeyByExternalReferenceCode(
-						scopeKey, objectEntry.getExternalReferenceCode(),
-						objectEntry);
-			}
-		}
-
-		if (unsafeFunction == null) {
-			throw new NotSupportedException(
-				"Create strategy \"" + createStrategy + "\" is not supported");
-		}
-
-		contextBatchUnsafeBiConsumer.accept(objectEntries, unsafeFunction);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
