0400f9df0ebd1f975a4410973e325c0466ad5e7e
Thiago Buarque <thiago.buarque@liferay.com>
2026-01-05 11:26:39 -0300

    LPD-75157 SF

--- modules/apps/product-navigation/product-navigation-control-menu-impl/src/main/java/com/liferay/product/navigation/control/menu/internal/manager/ProductNavigationControlMenuManagerImpl.java
+++ modules/apps/product-navigation/product-navigation-control-menu-impl/src/main/java/com/liferay/product/navigation/control/menu/internal/manager/ProductNavigationControlMenuManagerImpl.java
@@ -59,8 +59,8 @@ public class ProductNavigationControlMenuManagerImpl
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
-		boolean hasControlMenuEntries = _hasControlMenuEntries(
-			httpServletRequest);
+		boolean hasControlMenuEntries =
+			_hasRelevantProductNavigationControlMenuEntries(httpServletRequest);
 
 		Group group = themeDisplay.getScopeGroup();
 		Layout layout = themeDisplay.getLayout();
@@ -108,7 +108,7 @@ public class ProductNavigationControlMenuManagerImpl
 		return hasControlMenuEntries;
 	}
 
-	private boolean _hasControlMenuEntries(
+	private boolean _hasRelevantProductNavigationControlMenuEntries(
 		HttpServletRequest httpServletRequest) {
 
 		List<ProductNavigationControlMenuCategory>
