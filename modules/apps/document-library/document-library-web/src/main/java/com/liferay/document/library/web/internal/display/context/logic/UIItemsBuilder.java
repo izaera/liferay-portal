d9f41ea526591baa20268a72c5106a648ea0999f
Akhash R <akhash.ramprakash@liferay.com>
2026-01-12 12:18:21 +0530

    LPD-75744 Revert "LPD-70979 disable checkout action when workflow is enabled"
    This reverts commit cfd373e852c7a2174bd6c3b1640eb68b79031c27.
    
    (cherry picked from commit 092a8efb38e9f3b2e9edc0a646d0741499d43f47)

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/logic/UIItemsBuilder.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/logic/UIItemsBuilder.java
@@ -45,7 +45,6 @@ import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.WorkflowInstanceLink;
 import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
@@ -59,7 +58,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileShortcut;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.service.WorkflowInstanceLinkLocalServiceUtil;
 import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.theme.PortletDisplay;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -181,22 +179,7 @@ public class UIItemsBuilder {
 	}
 
 	public DropdownItem createCheckoutDropdownItem() {
-		return DropdownItemBuilder.setDisabled(
-			() -> {
-				WorkflowInstanceLink workflowInstanceLink =
-					WorkflowInstanceLinkLocalServiceUtil.
-						fetchWorkflowInstanceLink(
-							_fileEntry.getCompanyId(), _fileEntry.getGroupId(),
-							DLFileEntry.class.getName(),
-							_fileVersion.getFileVersionId());
-
-				if (workflowInstanceLink != null) {
-					return true;
-				}
-
-				return false;
-			}
-		).setHref(
+		return DropdownItemBuilder.setHref(
 			PortletURLBuilder.create(
 				_getActionURL(
 					"/document_library/edit_file_entry", Constants.CHECKOUT)
