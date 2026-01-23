aee9179ea72370714874f3fd82846db0b19b053a
Brian Chan <brian.chan@liferay.com>
2026-01-23 15:04:12 +0800

    LPD-76410 SF

--- modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/manager/UserManagerImpl.java
+++ modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/manager/UserManagerImpl.java
@@ -349,12 +349,12 @@ public class UserManagerImpl implements UserManager {
 		PermissionChecker permissionChecker =
 			PermissionCheckerFactoryUtil.create(portalUser);
 
+		Predicate predicate = null;
+
 		ExpandoColumn expandoColumn = _expandoColumnLocalService.getColumn(
 			CompanyThreadLocal.getCompanyId(),
 			com.liferay.portal.kernel.model.User.class.getName(),
 			ExpandoTableConstants.DEFAULT_TABLE_NAME, "deletedBySCIM");
-
-		Predicate predicate = null;
 
 		if (expandoColumn != null) {
 			predicate = UserTable.INSTANCE.userId.notIn(
@@ -1033,7 +1033,7 @@ public class UserManagerImpl implements UserManager {
 			return false;
 		}
 
-		int countScimDeletionExpandoValue = _userLocalService.dslQueryCount(
+		int count = _userLocalService.dslQueryCount(
 			DSLQueryFactoryUtil.count(
 			).from(
 				ExpandoValueTable.INSTANCE
@@ -1046,7 +1046,7 @@ public class UserManagerImpl implements UserManager {
 				)
 			));
 
-		if (countScimDeletionExpandoValue > 0) {
+		if (count > 0) {
 			return true;
 		}
 
