c90a07a37e514b9d655f62e9058116921d79b17f
Thiago Buarque <thiago.buarque@liferay.com>
2026-01-26 09:39:02 -0300

    LPD-75854 update test: account for the companyId property

--- modules/apps/static/portal-file-install/portal-file-install-test/src/testIntegration/java/com/liferay/portal/file/install/deploy/test/FileInstallDeployTest.java
+++ modules/apps/static/portal-file-install/portal-file-install-test/src/testIntegration/java/com/liferay/portal/file/install/deploy/test/FileInstallDeployTest.java
@@ -385,6 +385,18 @@ public class FileInstallDeployTest {
 								dictionaryKey, StringPool.EQUAL,
 								StringPool.QUOTE, dictionaryValue,
 								StringPool.QUOTE);
+
+							if (dictionaryKey.equals(
+									ExtendedObjectClassDefinition.Scope.GROUP.
+										getPropertyKey())) {
+
+								content = StringBundler.concat(
+									content, StringPool.RETURN_NEW_LINE,
+									ExtendedObjectClassDefinition.Scope.COMPANY.
+										getPropertyKey(),
+									StringPool.EQUAL, StringPool.QUOTE,
+									_company.getCompanyId(), StringPool.QUOTE);
+							}
 						}
 
 						Files.write(path, content.getBytes());
@@ -413,6 +425,18 @@ public class FileInstallDeployTest {
 							content, StringPool.RETURN_NEW_LINE, dictionaryKey,
 							StringPool.EQUAL, StringPool.QUOTE, dictionaryValue,
 							StringPool.QUOTE);
+
+						if (dictionaryKey.equals(
+								ExtendedObjectClassDefinition.Scope.GROUP.
+									getPropertyKey())) {
+
+							content = StringBundler.concat(
+								content, StringPool.RETURN_NEW_LINE,
+								ExtendedObjectClassDefinition.Scope.COMPANY.
+									getPropertyKey(),
+								StringPool.EQUAL, StringPool.QUOTE,
+								_company.getCompanyId(), StringPool.QUOTE);
+						}
 					}
 
 					Files.write(path, content.getBytes());
