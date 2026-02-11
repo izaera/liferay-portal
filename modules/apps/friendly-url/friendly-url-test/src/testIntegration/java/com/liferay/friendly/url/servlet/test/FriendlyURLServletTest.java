c95b93f8846713b2031d65945122d376e519c72e
Alan Huang <alan.huang@liferay.com>
2026-02-11 12:23:46 +0800

    LPD-78327 Auto SF

--- modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/servlet/test/FriendlyURLServletTest.java
+++ modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/servlet/test/FriendlyURLServletTest.java
@@ -1063,7 +1063,8 @@ public class FriendlyURLServletTest {
 			getPath(group, layoutUtilityPageEntryLayout),
 			_redirectConstructor1.newInstance(expectedURL));
 
-		Assert.assertEquals(expectedStatus, mockHttpServletResponse.getStatus());
+		Assert.assertEquals(
+			expectedStatus, mockHttpServletResponse.getStatus());
 	}
 
 	private void _testServiceRedirectWithRedirectEntry(
