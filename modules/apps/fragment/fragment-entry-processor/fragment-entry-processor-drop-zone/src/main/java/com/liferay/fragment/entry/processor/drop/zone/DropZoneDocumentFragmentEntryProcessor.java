da339b7c4e79a45359a861e7bbd8c5553228a5e6
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-02-12 09:41:37 +0100

    LPD-79100 Exclude the "data-lfr-" attributes

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-drop-zone/src/main/java/com/liferay/fragment/entry/processor/drop/zone/DropZoneDocumentFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-drop-zone/src/main/java/com/liferay/fragment/entry/processor/drop/zone/DropZoneDocumentFragmentEntryProcessor.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.LinkedHashMap;
@@ -224,6 +225,12 @@ public class DropZoneDocumentFragmentEntryProcessor
 				Element dropZoneElement = new Element("div");
 
 				for (Attribute attribute : element.attributes()) {
+					if (StringUtil.startsWith(
+							attribute.getKey(), "data-lfr-")) {
+
+						continue;
+					}
+
 					dropZoneElement.attr(
 						attribute.getKey(), attribute.getValue());
 				}
