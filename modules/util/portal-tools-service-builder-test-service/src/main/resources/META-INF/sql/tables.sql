d128a5e5fdbb7485b61591d005ada68d20e2849e
Eric Yan <eric.yan@liferay.com>
2026-02-17 16:59:30 -0800

    LPD-58634 Regen

--- modules/util/portal-tools-service-builder-test-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/util/portal-tools-service-builder-test-service/src/main/resources/META-INF/sql/tables.sql
@@ -280,6 +280,8 @@ create table NullConvertibleEntry (
 create table PermissionCheckFinderEntry (
 	permissionCheckFinderEntryId LONG not null primary key,
 	groupId LONG,
+	companyId LONG,
+	userId LONG,
 	integer_ INTEGER,
 	name VARCHAR(75) null,
 	type_ VARCHAR(75) null
