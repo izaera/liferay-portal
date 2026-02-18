5b502ebaee868092d8eb20163a3810a9dcc0faf8
Akhash R <akhash.ramprakash@liferay.com>
2026-02-18 21:40:54 +0530

    LPD-79507 Add test
    (cherry picked from commit d897b33365bd38f25b4e408a19c7413aa1da7179)

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/portlet/action/test/PublishFragmentEntryMVCActionCommandTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/portlet/action/test/PublishFragmentEntryMVCActionCommandTest.java
@@ -6,17 +6,31 @@
 package com.liferay.fragment.internal.portlet.action.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.fragment.configuration.FragmentServiceConfiguration;
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.service.FragmentCollectionLocalService;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.layout.util.UpdateLayoutStatusThreadLocal;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporarySwapper;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
@@ -27,6 +41,7 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -34,6 +49,7 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -113,6 +129,118 @@ public class PublishFragmentEntryMVCActionCommandTest {
 				LocaleUtil.getSiteDefault(), jsonObject.getString("error")));
 	}
 
+	@Test
+	@TestInfo("LPD-79507")
+	public void testPublishFragmentWithInvalidDropZoneConfiguration()
+		throws Exception {
+
+		try (CompanyConfigurationTemporarySwapper
+				companyConfigurationTemporarySwapper =
+					new CompanyConfigurationTemporarySwapper(
+						TestPropsValues.getCompanyId(),
+						FragmentServiceConfiguration.class.getName(),
+						HashMapDictionaryBuilder.<String, Object>put(
+							"propagateChanges", true
+						).build())) {
+
+			FragmentCollection fragmentCollection =
+				_fragmentCollectionLocalService.addFragmentCollection(
+					null, TestPropsValues.getUserId(), _group.getGroupId(),
+					RandomTestUtil.randomString(),
+					RandomTestUtil.randomString(), null);
+
+			String html =
+				"<div><lfr-drop-zone data-lfr-drop-zone-id=\"1\">" +
+					"</lfr-drop-zone></div>";
+
+			FragmentEntry fragmentEntry =
+				_fragmentEntryLocalService.addFragmentEntry(
+					null, TestPropsValues.getUserId(), _group.getGroupId(),
+					fragmentCollection.getFragmentCollectionId(),
+					StringPool.BLANK, RandomTestUtil.randomString(),
+					StringPool.BLANK, html, StringPool.BLANK, false,
+					StringPool.BLANK, null, 0, false, false,
+					FragmentConstants.TYPE_COMPONENT, null,
+					WorkflowConstants.STATUS_DRAFT,
+					ServiceContextTestUtil.getServiceContext(
+						_group, TestPropsValues.getUserId()));
+
+			Layout layout = LayoutTestUtil.addTypeContentPublishedLayout(
+				_group, RandomTestUtil.randomString(),
+				WorkflowConstants.STATUS_APPROVED);
+
+			long segmentsExperienceId =
+				_segmentsExperienceLocalService.
+					fetchDefaultSegmentsExperienceId(layout.getPlid());
+
+			FragmentEntryLink fragmentEntryLink =
+				_fragmentEntryLinkLocalService.addFragmentEntryLink(
+					null, TestPropsValues.getUserId(), _group.getGroupId(),
+					null, fragmentEntry.getExternalReferenceCode(),
+					fragmentEntry.getScopeERC(), segmentsExperienceId,
+					layout.getPlid(), fragmentEntry.getCss(),
+					fragmentEntry.getHtml(), fragmentEntry.getJs(),
+					"{fieldSets:[]}", StringPool.BLANK, StringPool.BLANK, 0,
+					null, fragmentEntry.getType(),
+					ServiceContextTestUtil.getServiceContext(
+						_group, TestPropsValues.getUserId()));
+
+			LayoutPageTemplateStructure layoutPageTemplateStructure =
+				_layoutPageTemplateStructureLocalService.
+					fetchLayoutPageTemplateStructure(
+						_group.getGroupId(), layout.getPlid());
+
+			LayoutStructure layoutStructure = LayoutStructure.of(
+				layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
+
+			LayoutStructureItem rootItem =
+				layoutStructure.getLayoutStructureItem(
+					layoutStructure.getMainItemId());
+
+			layoutStructure.addFragmentStyledLayoutStructureItem(
+				fragmentEntryLink.getFragmentEntryLinkId(),
+				rootItem.getItemId(), 0);
+
+			try (SafeCloseable safeCloseable =
+					UpdateLayoutStatusThreadLocal.
+						setUpdateLayoutStatusWithSafeCloseable(false)) {
+
+				_layoutPageTemplateStructureLocalService.
+					updateLayoutPageTemplateStructureData(
+						TestPropsValues.getUserId(), _group.getGroupId(),
+						layout.getPlid(), segmentsExperienceId,
+						layoutStructure.toString());
+			}
+
+			fragmentEntry.setHtml(html + "<!--updated-->");
+
+			fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+				fragmentEntry);
+
+			Assert.assertNotNull(layout.fetchDraftLayout());
+
+			MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+				new MockLiferayPortletActionRequest();
+
+			mockLiferayPortletActionRequest.setParameter(
+				"fragmentEntryId",
+				String.valueOf(fragmentEntry.getFragmentEntryId()));
+
+			mockLiferayPortletActionRequest.setAttribute(
+				WebKeys.THEME_DISPLAY, _getThemeDisplay());
+
+			_mvcActionCommand.processAction(
+				mockLiferayPortletActionRequest,
+				new MockLiferayPortletActionResponse());
+
+			Layout updatedLayout = _layoutLocalService.getLayout(
+				layout.getPlid());
+
+			Assert.assertEquals(
+				WorkflowConstants.STATUS_APPROVED, updatedLayout.getStatus());
+		}
+	}
+
 	private ThemeDisplay _getThemeDisplay() throws Exception {
 		ThemeDisplay themeDisplay = new ThemeDisplay();
 
@@ -139,6 +267,9 @@ public class PublishFragmentEntryMVCActionCommandTest {
 	private FragmentCollectionLocalService _fragmentCollectionLocalService;
 
 	@Inject
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Inject
 	private FragmentEntryLocalService _fragmentEntryLocalService;
 
 	@DeleteAfterTestRun
@@ -150,7 +281,17 @@ public class PublishFragmentEntryMVCActionCommandTest {
 	@Inject
 	private Language _language;
 
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
 	@Inject(filter = "mvc.command.name=/fragment/publish_fragment_entry")
 	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
 
 }
