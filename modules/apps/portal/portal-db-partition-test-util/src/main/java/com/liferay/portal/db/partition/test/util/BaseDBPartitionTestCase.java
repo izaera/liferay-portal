ccd5ee1ff35945687a74a5ebec74599360041491
Jorge Avalos <jorge.avalos@liferay.com>
2026-02-24 11:35:06 -0800

    LPD-79448 adapt changes from LPD-75854

--- modules/apps/portal/portal-db-partition-test-util/src/main/java/com/liferay/portal/db/partition/test/util/BaseDBPartitionTestCase.java
+++ modules/apps/portal/portal-db-partition-test-util/src/main/java/com/liferay/portal/db/partition/test/util/BaseDBPartitionTestCase.java
@@ -253,8 +253,8 @@ public abstract class BaseDBPartitionTestCase {
 				PreparedStatement preparedStatement1 =
 					connection.prepareStatement(
 						"insert into Group_ (mvccVersion, ctCollectionId, " +
-							"companyId, groupId, classNameId, classPK) " +
-								"values (?, ?, ?, ?, ?, ?)");
+							"companyId, groupId, classNameId, classPK, " +
+								"groupKey) values (?, ?, ?, ?, ?, ?, ?)");
 				PreparedStatement preparedStatement2 =
 					connection.prepareStatement(
 						"insert into PasswordPolicy (mvccVersion, " +
@@ -278,6 +278,7 @@ public abstract class BaseDBPartitionTestCase {
 				preparedStatement1.setLong(
 					5, ClassNameLocalServiceUtil.getClassNameId(Company.class));
 				preparedStatement1.setLong(6, companyId);
+				preparedStatement1.setString(7, RandomTestUtil.randomString());
 
 				preparedStatement1.executeUpdate();
 
