4fc14c64162167e579cdd4ea74eccb5512fbaab6
Victor Galan <victor.galan@liferay.com>
2026-02-27 10:37:56 +0100

    LPD-80741 Disable fragment button to avoid several requests

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/fragment-editor/FragmentEditor.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/fragment-editor/FragmentEditor.js
@@ -93,6 +93,8 @@ const FragmentEditor = ({
 	]);
 
 	const publish = () => {
+		setChangesStatus(CHANGES_STATUS.saving);
+
 		const formData = new FormData();
 
 		formData.append(`${namespace}fragmentEntryId`, fragmentEntryId);
