8f3d4fe56be921dcb9cf9d66025d8350b096edba
Lianne Louie <lianne.louie@liferay.com>
2026-02-27 03:35:51 -0800

    LPD-79569 update summary to use localized fields as well

--- modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/UserModelSearchConfigurator.java
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/UserModelSearchConfigurator.java
@@ -9,6 +9,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.IndexWriterHelper;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Localization;
 import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
 import com.liferay.portal.search.indexer.IndexerDocumentBuilder;
 import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
@@ -64,6 +65,8 @@ public class UserModelSearchConfigurator
 		_modelIndexWriterContributor = new UserModelIndexerWriterContributor(
 			_indexerDocumentBuilder, _indexWriterHelper,
 			_dynamicQueryBatchIndexingActionableFactory, _userLocalService);
+		_modelSummaryContributor = new UserModelSummaryContributor(
+			_localization);
 	}
 
 	@Reference
@@ -78,9 +81,11 @@ public class UserModelSearchConfigurator
 	@Reference
 	private IndexWriterHelper _indexWriterHelper;
 
+	@Reference
+	private Localization _localization;
+
 	private ModelIndexerWriterContributor<User> _modelIndexWriterContributor;
-	private final ModelSummaryContributor _modelSummaryContributor =
-		new UserModelSummaryContributor();
+	private ModelSummaryContributor _modelSummaryContributor;
 
 	@Reference
 	private UserLocalService _userLocalService;
