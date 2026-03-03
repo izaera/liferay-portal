0b1af08f15912bd40aa3fc778c83b5aae6eba245
Brian Chan <brian.chan@liferay.com>
2026-03-03 07:10:33 -0300

    LPD-80947 Rename

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/web/internal/object/entries/portlet/action/test/UploadAttachmentMVCActionCommandTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/web/internal/object/entries/portlet/action/test/UploadAttachmentMVCActionCommandTest.java
@@ -170,7 +170,7 @@ public class UploadAttachmentMVCActionCommandTest {
 		_testProcessAction(RandomTestUtil.randomInt(1, 10));
 	}
 
-	private FileItem _getFileItem(int fileSize) throws Exception {
+	private FileItem _getFileItem(int size) throws Exception {
 		Path path = Files.createTempFile(null, ".txt");
 
 		Files.write(path, "".getBytes());
@@ -194,7 +194,7 @@ public class UploadAttachmentMVCActionCommandTest {
 				}
 
 				public long getSize() {
-					return fileSize;
+					return size;
 				}
 
 				public File getStoreLocation() {
@@ -240,7 +240,7 @@ public class UploadAttachmentMVCActionCommandTest {
 		return mockHttpServletRequest;
 	}
 
-	private void _testProcessAction(int fileSize) throws Exception {
+	private void _testProcessAction(int size) throws Exception {
 		ReflectionTestUtil.setFieldValue(
 			_uploadHandler, "_portal",
 			ProxyUtil.newProxyInstance(
@@ -258,7 +258,7 @@ public class UploadAttachmentMVCActionCommandTest {
 							_getMockHttpServletRequest(
 								_objectField.getObjectFieldId()),
 							HashMapBuilder.put(
-								"file", new FileItem[] {_getFileItem(fileSize)}
+								"file", new FileItem[] {_getFileItem(size)}
 							).build(),
 							new HashMap<>()),
 						null, RandomTestUtil.randomString());
