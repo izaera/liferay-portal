075dd96b0b14a7b703c7e560780bc03a34f4512c
Eric Yan <eric.yan@liferay.com>
2026-03-05 11:08:35 -0800

    LPD-77695 Create default instance once, prep next

--- portal-impl/src/com/liferay/portal/events/GlobalStartupAction.java
+++ portal-impl/src/com/liferay/portal/events/GlobalStartupAction.java
@@ -20,8 +20,6 @@ import com.liferay.portal.util.BrowserLauncher;
 import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 
-import java.io.File;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -69,11 +67,7 @@ public class GlobalStartupAction extends SimpleAction {
 		// Auto deploy
 
 		try {
-			File deployDir = new File(PropsValues.AUTO_DEPLOY_DEPLOY_DIR);
-			long interval = PropsValues.AUTO_DEPLOY_INTERVAL;
-
-			AutoDeployDir autoDeployDir = new AutoDeployDir(
-				AutoDeployDir.DEFAULT_NAME, deployDir, interval);
+			AutoDeployDir autoDeployDir = AutoDeployDir.getDefault();
 
 			if (PropsValues.AUTO_DEPLOY_ENABLED) {
 				if (_log.isInfoEnabled()) {
