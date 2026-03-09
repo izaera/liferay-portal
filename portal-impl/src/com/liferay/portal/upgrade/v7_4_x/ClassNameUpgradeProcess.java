78aa7996844c934492ab13fd5c01cecc2ffe4e19
Marco Galluzzi <marco.galluzzi@liferay.com>
2026-03-09 12:49:46 +0000

    LPD-80354 Delete statement is not portable so I'm splitting it into select+delete
    (cherry picked from commit 6facb676ed3a9455f045d36f70f1cf6b6ed7d0fa)

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/ClassNameUpgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/ClassNameUpgradeProcess.java
@@ -6,9 +6,10 @@
 package com.liferay.portal.upgrade.v7_4_x;
 
 import com.liferay.document.library.kernel.processor.RawMetadataProcessor;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringBundler;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -138,18 +139,33 @@ public class ClassNameUpgradeProcess extends UpgradeProcess {
 			long newStructureId, long oldStructureId)
 		throws Exception {
 
-		try (PreparedStatement preparedStatement = connection.prepareStatement(
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
 				StringBundler.concat(
-					"delete from DLFileEntryMetadata d1 where DDMStructureId ",
-					"= ? and exists (select 1 from DLFileEntryMetadata d2 ",
-					"where d1.ctCollectionId = d2.ctCollectionId and ",
-					"d2.DDMStructureId = ? and d1.fileVersionId = ",
-					"d2.fileVersionId)"))) {
+					"select d1.fileEntryMetadataId from DLFileEntryMetadata ",
+					"d1 where d1.DDMStructureId = ? and exists (select 1 from ",
+					"DLFileEntryMetadata d2 where d1.ctCollectionId = ",
+					"d2.ctCollectionId and d2.DDMStructureId = ? and ",
+					"d1.fileVersionId = d2.fileVersionId)"))) {
 
-			preparedStatement.setLong(1, oldStructureId);
-			preparedStatement.setLong(2, newStructureId);
+			preparedStatement1.setLong(1, oldStructureId);
+			preparedStatement1.setLong(2, newStructureId);
 
-			preparedStatement.execute();
+			try (PreparedStatement preparedStatement2 =
+					AutoBatchPreparedStatementUtil.autoBatch(
+						connection,
+						"delete from DLFileEntryMetadata where " +
+							"fileEntryMetadataId = ?");
+				ResultSet resultSet = preparedStatement1.executeQuery()) {
+
+				while (resultSet.next()) {
+					preparedStatement2.setLong(
+						1, resultSet.getLong("fileEntryMetadataId"));
+
+					preparedStatement2.addBatch();
+				}
+
+				preparedStatement2.executeBatch();
+			}
 		}
 	}
 
