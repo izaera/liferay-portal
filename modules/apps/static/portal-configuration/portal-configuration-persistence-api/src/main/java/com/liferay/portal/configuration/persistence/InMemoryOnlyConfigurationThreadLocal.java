b8f4d113835ee6a756dfb2a49dfb0ff5d29e5b6e
Drew Brokke <drew.brokke@liferay.com>
2026-03-12 15:30:38 -0500

    LPD-73273 (unique) portal-configuration-persistence-api: implement InMemoryOnlyConfigurationThreadLocal.setInMemoryOnlyWithSafeCloseable to match final state of master

--- modules/apps/static/portal-configuration/portal-configuration-persistence-api/src/main/java/com/liferay/portal/configuration/persistence/InMemoryOnlyConfigurationThreadLocal.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-api/src/main/java/com/liferay/portal/configuration/persistence/InMemoryOnlyConfigurationThreadLocal.java
@@ -6,6 +6,7 @@
 package com.liferay.portal.configuration.persistence;
 
 import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.petra.lang.SafeCloseable;
 
 /**
  * @author Raymond Augé
@@ -16,11 +17,13 @@ public class InMemoryOnlyConfigurationThreadLocal {
 		return _inMemoryOnly.get();
 	}
 
-	public static void setInMemoryOnly(boolean inMemoryOnly) {
-		_inMemoryOnly.set(inMemoryOnly);
+	public static SafeCloseable setInMemoryOnlyWithSafeCloseable(
+		boolean inMemoryOnly) {
+
+		return _inMemoryOnly.setWithSafeCloseable(inMemoryOnly);
 	}
 
-	private static final ThreadLocal<Boolean> _inMemoryOnly =
+	private static final CentralizedThreadLocal<Boolean> _inMemoryOnly =
 		new CentralizedThreadLocal<>(
 			InMemoryOnlyConfigurationThreadLocal.class + "._inMemoryOnly",
 			() -> Boolean.FALSE, false);
