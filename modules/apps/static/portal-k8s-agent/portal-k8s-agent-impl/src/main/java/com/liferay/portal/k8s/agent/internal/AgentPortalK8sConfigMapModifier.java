a2a86837333a44fbc05c340c6596c237670c1229
Drew Brokke <drew.brokke@liferay.com>
2026-03-12 15:32:30 -0500

    LPD-73273 (unique) portal-k8s-agent-impl: update usage

--- modules/apps/static/portal-k8s-agent/portal-k8s-agent-impl/src/main/java/com/liferay/portal/k8s/agent/internal/AgentPortalK8sConfigMapModifier.java
+++ modules/apps/static/portal-k8s-agent/portal-k8s-agent-impl/src/main/java/com/liferay/portal/k8s/agent/internal/AgentPortalK8sConfigMapModifier.java
@@ -6,6 +6,7 @@
 package com.liferay.portal.k8s.agent.internal;
 
 import com.liferay.osgi.util.configuration.ConfigurationFactoryUtil;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -546,8 +547,9 @@ public class AgentPortalK8sConfigMapModifier
 		String virtualInstancePid = _getVirtualInstancePid(
 			config, virtualInstanceId);
 
-		try {
-			InMemoryOnlyConfigurationThreadLocal.setInMemoryOnly(true);
+		try (SafeCloseable safeCloseable =
+				InMemoryOnlyConfigurationThreadLocal.
+					setInMemoryOnlyWithSafeCloseable(true)) {
 
 			Configuration configuration = null;
 
@@ -616,9 +618,6 @@ public class AgentPortalK8sConfigMapModifier
 
 			configuration.addAttributes(
 				Configuration.ConfigurationAttribute.READ_ONLY);
-		}
-		finally {
-			InMemoryOnlyConfigurationThreadLocal.setInMemoryOnly(false);
 		}
 	}
 
