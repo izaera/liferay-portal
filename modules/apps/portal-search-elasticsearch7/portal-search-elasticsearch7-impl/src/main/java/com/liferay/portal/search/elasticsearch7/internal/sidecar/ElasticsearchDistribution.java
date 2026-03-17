cac3e4fb45aef4bd5f81db77214ea959eccac0b8
Felipe Lorenz <felipe.lorenz@hotmail.com>
2026-03-17 10:30:35 -0300

    LPD-74525 portal-search-elasticsearch7-impl: Update sidecar distribution to 7.17.29
    https://liferay.atlassian.net/browse/LPD-74526

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchDistribution.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/ElasticsearchDistribution.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public class ElasticsearchDistribution implements Distribution {
 
-	public static final String VERSION = "7.17.26";
+	public static final String VERSION = "7.17.29";
 
 	@Override
 	public Distributable getElasticsearchDistributable() {
@@ -46,23 +46,23 @@ public class ElasticsearchDistribution implements Distribution {
 	}
 
 	private static final String _ELASTICSEARCH_CHECKSUM =
-		"5b32714d5683059c213fbe610de21d1016048e97ee4215ae4104aa6065d409c33d7c" +
-			"e13b5e1443b559ea2167f7ebe8a5b26609ff1e085e4f4995bf46083afa35";
+		"4ad566436ca792455c5afbdfd46908da3176d37f8ff427122bbb6ae23b7badb614e3" +
+			"126266cbbfc266a832f6d20126626c331ca318394bcc7285d8009ecc5843";
 
 	private static final String _ICU_CHECKSUM =
-		"68f1024daea400b100db8a4a6ec81ad5b9df76f1ef60f98cad06389a9aea58ee21b2" +
-			"939ba770f7d3c8e1901eab347cfd4d68590f85d730217271f17cd392bfc6";
+		"0fd03cdfda357d2358293801be04722df9650810cf1a7243ae57cb419ba5df0c2b4f" +
+			"5e84c8c368d373a9b40b768cc729e9fce14ecb3fe86b5426d470dab0233c";
 
 	private static final String _KUROMOJI_CHECKSUM =
-		"16226951b084dd4f33d640da87a69a132336e53e5537d441711080a594dd616b5c3f" +
-			"5c5b2e4b678afc06c8d3472d642018c258e4e07b105ad69ffb6b02f7892f";
+		"eda3f4d1a9eaae39d11694ebc88fe4b252c3966da961ef8d0bb3a4d99385e8710c28" +
+			"ec677b078a36991a37c2359e50859506c5d888de898079c15146c4035536";
 
 	private static final String _SMARTCN_CHECKSUM =
-		"df4babcd8a937fda74fb7c31f1a4d138cfb0c2e6a87f31e092a3193df5e138c9182e" +
-			"af521370178cb9b5b0a4dabacf56a5236c8d6927460d072c49cd46da1148";
+		"c80991543cc46e31b7dd10aafe135588c97c53c1ff109e6a7f83179d27d381a180ac" +
+			"076412270b52fed1270602d1da24a3b79aaf229fe5288f05c38e4b57d0e6";
 
 	private static final String _STEMPEL_CHECKSUM =
-		"524df432270e94ff2b21d3efc0f2a82a2bb2c0fd36c3dbe33333237dfff0c67eff4d" +
-			"de289bfc6f274959b7b544535f5ded49cd58ee9a2202c4bcd228756aef69";
+		"febecb297eb7ce918a7921a8624ab2113de806a949c632d26abe154703460d80395f" +
+			"7219df0812af68830eb39d6c01ef635ee65d6919aadbaf29788fef67f9b5";
 
 }
