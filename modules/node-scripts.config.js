47973438c6566850965f45854efcba6f6377d12d
Carlos Correa <carlos.correa@liferay.com>
2026-03-25 17:26:41 +0100

    LPD-81731 Update css in this branch, this is a manual commit for this branch, it comes from 5f66e15b7df68d47c1b73d506a1b6b30425447e2

--- modules/node-scripts.config.js
+++ modules/node-scripts.config.js
@@ -41,7 +41,7 @@ module.exports = {
 			'react-helmet',
 			'react-router-dom',
 			'graphiql',
-			'graphiql/graphiql.css',
+			'graphiql/style.css',
 			'axe-core',
 			'clipboard',
 			'cropperjs',
