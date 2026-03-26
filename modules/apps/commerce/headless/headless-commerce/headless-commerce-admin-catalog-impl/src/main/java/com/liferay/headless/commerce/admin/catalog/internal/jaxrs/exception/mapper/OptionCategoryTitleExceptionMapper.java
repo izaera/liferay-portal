15d23c68d13b8edfc62c2ea0f63384c4a317420f
Lianne Louie <lianne.louie@liferay.com>
2026-03-25 17:06:42 -0700

    LPD-79398 backport exclusive SF

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/jaxrs/exception/mapper/OptionCategoryTitleExceptionMapper.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/jaxrs/exception/mapper/OptionCategoryTitleExceptionMapper.java
@@ -9,10 +9,10 @@ import com.liferay.commerce.product.exception.CPOptionCategoryTitleException;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
 import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
 
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-
 import org.osgi.service.component.annotations.Component;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 
 /**
  * @author Andrea Sbarra
