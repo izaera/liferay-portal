18ebf78e77e2f33adc0661311240d73d87cbe6b7
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-03-27 10:25:26 +0100

    LPD-83356 Check modified TypeScript projects only
    This reverts commit 1f897bbcd6dd73bb104c5b51ca62100deed574da and makes
    CI check ALL TypeScript project for ALL pull requests.
    
    This relies on CI caching tsbuildinfo files so that these checks are
    performed in a reasonable time.

--- modules/frontend-sdk/node-scripts/util/format/formatPortal.mjs
+++ modules/frontend-sdk/node-scripts/util/format/formatPortal.mjs
@@ -4,7 +4,6 @@
  */
 
 import getProjectDirs from '../getProjectDirs.mjs';
-import getTypeScriptProjectDirs from '../getTypeScriptProjectDirs.mjs';
 import {PLAYWRIGHT_DIR} from '../locations.mjs';
 import formatAPISubmodules from './formatters/formatAPISubmodules.mjs';
 import formatConfigFileNames from './formatters/formatConfigFileNames.mjs';
@@ -86,20 +85,17 @@ export default async function formatPortal(check, files) {
 				file.endsWith('.tsx')
 		)
 	) {
-		let projectDirs;
-
-		if (files) {
-			projectDirs = await getTypeScriptProjectDirs(files);
-		}
-		else {
-			projectDirs = await getProjectDirs();
-		}
 
 		// We check all projects no matter if formatting current branch, local
 		// changes or everything because a change in one project may break
 		// others
 
-		if (!(await formatTypeScript([...projectDirs, PLAYWRIGHT_DIR]))) {
+		if (
+			!(await formatTypeScript([
+				...(await getProjectDirs()),
+				PLAYWRIGHT_DIR,
+			]))
+		) {
 			checksPassed = false;
 		}
 	}
