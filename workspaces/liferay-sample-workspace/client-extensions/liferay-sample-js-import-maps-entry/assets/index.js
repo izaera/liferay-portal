61c607d35013ce30d771e05b93dafce5ed3342d5
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-06-10 14:03:26 +0200

    LPD-XXXXX Tweak sample workspace demo
    I'm using the JS Import Maps CX because it lets me deploy a custom
    JavaScript file to emulate what the Audiences Servlet would be producing
    under the hood.

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-js-import-maps-entry/assets/index.js
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-js-import-maps-entry/assets/index.js
@@ -3,6 +3,41 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import jquery from 'jquery';
+import {audiences} from '/o/frontend-js-audiences-web/__liferay__/index.js';
 
-export default jquery;
+const AUDIENCE_ID = '35137';
+
+audiences.on(
+	AUDIENCE_ID,
+	function showFirefoxMessage() {
+		const MESSAGE_ID = 'firefox_message';
+
+		// If the message is already present, don't do anything (to avoid multiple personalizations
+		// due to SPA navigations).
+		const currentMessage = document.getElementById(MESSAGE_ID);
+
+		if (currentMessage) return;
+
+		// Look for the banner where we will add the language hint
+		const div = document.getElementById('banner');
+
+		if (!div) return;
+
+		// Add the hint
+		const message = document.createElement('div');
+
+		message.id = MESSAGE_ID;
+		message.style.color = 'white';
+		message.style.backgroundColor = '#0000C0';
+		message.style.paddingBottom = '1em';
+		message.style.paddingTop = '1em';
+		message.style.textAlign = 'center';
+
+		message.innerHTML = `
+			Hey, we see you are using Firefox 🦊! We welcome you as a FOSS supporter! 🎉
+		`;
+
+		div.appendChild(message);
+	}
+);
+
